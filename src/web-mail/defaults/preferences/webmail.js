db4d9250f1e449702e6f53655634789215877105
archer <archer>
2005-04-18 20:44:33 +0000

    added imap port number

--- src/web-mail/defaults/preferences/webmail.js
+++ src/web-mail/defaults/preferences/webmail.js
@@ -1,4 +1,5 @@
 pref("webmail.server.port.pop", 110);
+pref("webmail.server.port.imap", 143);
 pref("webmail.logging.comms.UseLog", false);
 pref("webmail.logging.comms.UseJSconsole", true);
 pref("webmail.logging.comms.UseFile", false);
