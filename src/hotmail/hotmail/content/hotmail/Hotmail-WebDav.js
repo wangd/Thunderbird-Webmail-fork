37d1d4cdeeafc06864ccde0f7e3ea4504981e15a
archer <archer>
2005-05-03 21:17:11 +0000

    fixed download bug

--- src/hotmail/hotmail/content/hotmail/Hotmail-WebDav.js
+++ src/hotmail/hotmail/content/hotmail/Hotmail-WebDav.js
@@ -646,8 +646,8 @@ HotmailWebDav.prototype =
             //server response
             var szMsg =  szResponse;
             szMsg = szMsg.replace(/^\./mg,"..");    //bit padding 
-            if (szMsg.lastIndexOf("\r\n") == -1) szMsg += "\r\n";
-            szMsg += ".\r\n";  //msg end 
+            szMsg += "\r\n.\r\n";//msg end 
+            
                                                                                                               
             var szPOPResponse = "+OK " + szMsg.length + "\r\n";                     
             szPOPResponse += szMsg;
