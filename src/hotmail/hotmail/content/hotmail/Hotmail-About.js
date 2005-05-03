b0a7ea63f0c57bba6c066d6977f0e1ca5b36bc05
archer <archer>
2005-05-03 21:40:40 +0000

    added localization flags

--- src/hotmail/hotmail/content/hotmail/Hotmail-About.js
+++ src/hotmail/hotmail/content/hotmail/Hotmail-About.js
@@ -5,12 +5,35 @@ function AddNamesHotmailListItems()
     var list = document.getElementById("HotmailContributorBox");
    
     var aszNames = szContributors.split(";");
-    
+      
     for(i =0 ; i< aszNames.length; i++)
     {
+        var hBox = document.createElement("hbox");
+        
+        var aData = aszNames[i].split("|");
+        var szName = aData[0];
+        var szNum = -1;
+        try
+        {
+            szNum = aData[1];
+        }
+        catch(e)
+        {
+            szNum = -1;
+        }
+        
+        
+        var flagImage = document.createElement("image");
+        flagImage.setAttribute("id", "flagImage");
+        flagImage.setAttribute("class", "flag");
+        flagImage.setAttribute("value", szNum.toString());
+        hBox.appendChild(flagImage);
+        
         var label = document.createElement("label");
-        label.setAttribute("value", aszNames[i]);
+        label.setAttribute("value",szName); 
         label.setAttribute("class", "HotmailContributorText");
-        list.appendChild(label);
+        hBox.appendChild(label);
+        
+        list.appendChild(hBox);
     }
 }
