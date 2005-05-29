657466b60305a29f2fd483cf3876aa851a094b68
archer <archer>
2005-05-29 21:54:29 +0000

    added smtp default

--- src/web-mail/defaults/preferences/webmail.js
+++ src/web-mail/defaults/preferences/webmail.js
@@ -1,5 +1,6 @@
 pref("webmail.server.port.pop", 110);
 pref("webmail.server.port.imap", 143);
+pref("webmail.server.port.smtp", 25);
 pref("webmail.logging.comms.UseLog", false);
 pref("webmail.logging.comms.UseJSconsole", true);
 pref("webmail.logging.comms.UseFile", false);
