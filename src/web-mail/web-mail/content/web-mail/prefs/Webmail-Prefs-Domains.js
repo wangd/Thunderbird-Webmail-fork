1d829d407615866c4759aae170cc1e29f5fe2173
archer <archer>
2005-06-06 00:18:26 +0000

    updated for pref window V

--- src/web-mail/web-mail/content/web-mail/prefs/Webmail-Prefs-Domains.js
+++ src/web-mail/web-mail/content/web-mail/prefs/Webmail-Prefs-Domains.js
@@ -1,88 +1,88 @@
-function Startup()
+var gDomainsPane = 
 {
-    try
-    {
-        parent.gWebmailLog.Write("Webmail: Webmail-Prefs-Domains.js : Startup - START");
-        
-        AddDomainsListItems(LoadWebmailDomains());
-         
-        parent.gWebmailLog.Write("Webmail: Webmail-Prefs-Domains.js : Startup - END");
-    }
-    catch(e)
-    {
-        parent.gWebmailLog.DebugDump("Webmail: Webmail-Prefs-Domains.js : Exception in Startup : " 
-                                     + e.name + 
-                                     ".\nError message: " 
-                                     + e.message);
-    }
-}
-
-
-function LoadWebmailDomains()
-{
-    try
-    {
-        parent.gWebmailLog.Write("Webmail: Webmail-Prefs-Domains.js : LoadWebmailDomains - START");
-       
-        var DomainManager = Components.classes["@mozilla.org/DomainManager;1"].
-                                            getService().
-                                            QueryInterface(Components.interfaces.nsIDomainManager);
-        
-        var aszDomains = {value : null};
-        var iCount = {value : null }; 
-        if (DomainManager.getAllDomainsForProtocol("pop", iCount, aszDomains))
-        {
-            parent.gWebmailLog.Write("Webmail: Webmail-Prefs-Domains.js : LoadWebmailDomains "
-                                            + "pop"
-                                            + iCount.value +"\n"
-                                            + aszDomains.value);
-        }
-        
-        parent.gWebmailLog.Write("Webmail: Webmail-Prefs-Domains.js : LoadWebmailDomains - END"); 
-        return aszDomains.value;
-    }
-    catch(e)
-    {
-        parent.gWebmailLog.DebugDump("Webmail: Webmail-Prefs-Domains.js : Exception in LoadWebmailDomains : " 
-                                                 + e.name 
-                                                 + ".\nError message: " 
-                                                 + e.message);
-    }
+    m_DebugLog : null,
    
-}
-
-
-
-function AddDomainsListItems(Domains)
-{
-    try
+    init: function ()
     {
-        parent.gWebmailLog.Write("Webmail: Webmail-Prefs-Domains.js : AddListItems - START");
-        parent.gWebmailLog.Write("Webmail: Webmail-Prefs-Domains.js : AddListItems " + Domains);
+    
+        this.m_DebugLog = new DebugLog("webmail.logging.comms", 
+                                       "{3c8e8390-2cf6-11d9-9669-0800200c9a66}",
+                                       "webmailPrefs");
+                                   
+        this.m_DebugLog.Write("Webmail-Prefs-Domains : init - START");
         
-        if (Domains.length > 0) 
-        {
-            var list = document.getElementById("listView");
-            
-            for(i =0 ; i< Domains.length; i++)
-            {
-                var newItem = document.createElement("listitem");  
-                newItem.setAttribute("label", Domains[i]);
-               	list.appendChild(newItem);
-            }
-        }
-        else
-        {
-            parent.gWebmailLog.Write("Webmail: Webmail-Prefs-Domains.js : AddListItems - Domains = 0");
-        }
-                  
-        parent.gWebmailLog.Write("Webmail: Webmail-Prefs-Domains.js : AddListItems - END"); 
-    }
-    catch(e)
+        var aszDomains = this.loadWebmailDomains();
+        this.addDomainsListItems(aszDomains);
+        
+        this.m_DebugLog.Write("Webmail-Prefs-Domains : init - END");
+    },
+    
+    
+    
+    loadWebmailDomains : function ()
     {
-         parent.gWebmailLog.DebugDump("Webmail: Webmail-Prefs-Domains.js : Exception in AddListItems : " 
-                                       + e.name + 
-                                       ".\nError message: " 
-                                       + e.message);
-    }
-}
+        try
+        {
+            this.m_DebugLog.Write("Webmail-Prefs-Domains : LoadWebmailDomains - START");
+           
+            var DomainManager = Components.classes["@mozilla.org/DomainManager;1"].
+                                           getService().
+                                           QueryInterface(Components.interfaces.nsIDomainManager);
+            
+            var aszDomains = {value : null};
+            var iCount = {value : null }; 
+            if (DomainManager.getAllDomainsForProtocol("pop", iCount, aszDomains))
+            {
+                this.m_DebugLog.Write("Webmail-Prefs-Domains : LoadWebmailDomains "
+                                                + "pop"
+                                                + iCount.value +"\n"
+                                                + aszDomains.value);
+            }
+            
+            this.m_DebugLog.Write("Webmail-Prefs-Domains : LoadWebmailDomains - END"); 
+            return aszDomains.value;
+        }
+        catch(e)
+        {
+            this.m_DebugLog.DebugDump("Webmail-Prefs-Domains : Exception in LoadWebmailDomains : " 
+                                                     + e.name 
+                                                     + ".\nError message: " 
+                                                     + e.message);
+        }
+    },
+    
+    
+    addDomainsListItems : function (aszDomains)
+    {
+        try
+        {
+            this.m_DebugLog.Write("Webmail-Prefs-Domains : AddListItems - START");
+            
+            if (aszDomains.length > 0) 
+            {
+                aszDomains = aszDomains.sort()
+                var list = document.getElementById("listView");
+                
+                for(i =0 ; i< aszDomains.length; i++)
+                {
+                    var newItem = document.createElement("listitem");  
+                    newItem.setAttribute("label", aszDomains[i]);
+                   	list.appendChild(newItem);
+                }
+            }
+            else
+            {
+                this.m_DebugLog.Write("Webmail-Prefs-Domains : AddListItems - Domains = 0");
+            }
+                      
+            this.m_DebugLog.Write("Webmail-Prefs-Domains : AddListItems - END"); 
+        }
+        catch(e)
+        {
+            this.m_DebugLog.DebugDump("Webmail-Prefs-Domains : Exception in AddListItems : " 
+                                           + e.name + 
+                                           ".\nError message: " 
+                                           + e.message);
+        }
+    }, 
+};
