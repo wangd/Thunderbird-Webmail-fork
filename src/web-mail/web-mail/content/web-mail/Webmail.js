c51d13f1a3d8b2a0d2eaf91689ad44cd0a32eaf8
archer <archer>
2005-06-23 18:04:59 +0000

    cleaned up start code

--- src/web-mail/web-mail/content/web-mail/Webmail.js
+++ src/web-mail/web-mail/content/web-mail/Webmail.js
@@ -1,91 +1,110 @@
-//project globals
-var gWebmailDebugLog = null;
-var gPOP = null;
-var g_DomainManager = null;
-var g_AccountWizard = null;
+window.addEventListener("load",  function () {gWebMail.startUp();}, false);
+window.addEventListener("unload", function () {gWebMail.shutDown();},  false);
 
-window.addEventListener("load",   WebmailStartUp, false);
-window.addEventListener("unload", WebmailShutDown,  false);
-
-
-function WebmailStartUp()
-{   
-    try
-    {    
-        var scriptLoader =  Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
-                                        .getService(Components.interfaces.mozIJSSubScriptLoader);
-        if (scriptLoader)
-        {
-            scriptLoader.loadSubScript("chrome://web-mail/content/common/DebugLog.js");
-            scriptLoader.loadSubScript("chrome://web-mail/content/common/CommonPrefs.js");
-            scriptLoader.loadSubScript("chrome://web-mail/content/Webmail-AccountManager.js");  
-        }
-                
-        //create debug log global 
-        gWebmailDebugLog = new DebugLog("webmail.logging.comms",
-                                        "{3c8e8390-2cf6-11d9-9669-0800200c9a66}", 
-                                        "general");
-                                        
-        gWebmailDebugLog.Write("Webmail: Webmail.js : WebmailStartUp - START");
+var gWebMail =
+{
+    m_Log : null,
+    m_POP : null,
+    n_SMTP : null,
+    m_DomainManager : null,
+    m_AccountWizard : null,
     
-        //account wizard
-   	    var oPref = new Object();
-        oPref.Value = null;
-        var  WebMailPrefAccess = new WebMailCommonPrefAccess();
-        WebMailPrefAccess.Get("bool","webmail.UseAccountWizard",oPref); 
-        g_AccountWizard = new WebmailAccountManager();  //create webmail.rdf file
-        if (oPref.Value) g_AccountWizard.createISP();
+    startUp : function ()
+    {   
+        try
+        {
+            var scriptLoader =  Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+                                          .getService(Components.interfaces.mozIJSSubScriptLoader);
+            if (scriptLoader)
+            {
+                scriptLoader.loadSubScript("chrome://web-mail/content/common/DebugLog.js");
+                scriptLoader.loadSubScript("chrome://web-mail/content/common/CommonPrefs.js");
+                scriptLoader.loadSubScript("chrome://web-mail/content/Webmail-AccountManager.js");  
+            }
+            
+            //create debug log global 
+            this.m_Log = new DebugLog("webmail.logging.comms",
+                                      "{3c8e8390-2cf6-11d9-9669-0800200c9a66}", 
+                                      "general");
+                                        
+            this.m_Log.Write("Webmail.js : startUp - START");
+    
+            //account wizard
+       	    var oPref = new Object();
+            oPref.Value = null;
+            var  WebMailPrefAccess = new WebMailCommonPrefAccess();
+            WebMailPrefAccess.Get("bool","webmail.UseAccountWizard",oPref); 
+            this.m_AccountWizard = new WebmailAccountManager();  //create webmail.rdf file
+            if (oPref.Value) this.m_AccountWizard.createISP();
    	   
            
-        //start  service
-        try
-        {   //create service
-            g_DomainManager = Components.classes["@mozilla.org/DomainManager;1"]
-                                        .getService()
-                                        .QueryInterface(Components.interfaces.nsIDomainManager);
-            
-            gPOP = Components.classes["@mozilla.org/POPConnectionManager;1"]
-                             .getService()
-                             .QueryInterface(Components.interfaces.nsIPOPConnectionManager);
-            
-            if (gPOP.Start())
-                gWebmailDebugLog.Write("Webmail: Webmail.js : WebmailStartUp - pop server started");
-            else
-                gWebmailDebugLog.Write("Webmail: Webmail.js : WebmailStartUp - pop server not started");                      
+            //start  service
+            try
+            {   //create service
+                this.m_DomainManager = Components.classes["@mozilla.org/DomainManager;1"]
+                                            .getService()
+                                            .QueryInterface(Components.interfaces.nsIDomainManager);
+                
+                this.m_POP = Components.classes["@mozilla.org/POPConnectionManager;1"]
+                                 .getService()
+                                 .QueryInterface(Components.interfaces.nsIPOPConnectionManager);
+                
+                if (this.m_POP.Start())
+                    this.m_Log.Write("Webmail.js : startUp - pop server started");
+                else
+                    this.m_Log.Write("Webmail.js : startUp - pop server not started"); 
+                    
+                
+                this.m_SMTP = Components.classes["@mozilla.org/SMTPConnectionManager;1"]
+                                 .getService()
+                                 .QueryInterface(Components.interfaces.nsISMTPConnectionManager);
+                
+                if (this.m_SMTP.Start())
+                    this.m_Log.Write("Webmail.js : startUp - SMTP server started");
+                else
+                    this.m_Log.Write("Webmail.js : startUp - SMTP server not started");                      
+            }
+            catch(e)
+            {
+                this.m_Log.Write("Webmail.js :  Exception in startUp servers " 
+                                        + e.name + 
+                                        ".\nError message: " 
+                                        + e.message);
+            }
+        
+            window.removeEventListener("load",function() {gWebMail.startUp();}, false);
+                      
+    	    this.m_Log.Write("Webmail.js : startUp - END ");
         }
         catch(e)
         {
-            gWebmailDebugLog.Write("Webmail: Webmail.js : Starting POP servers Exception in WebmailStartUp " 
-                                    + e.name + 
-                                    ".\nError message: " 
-                                    + e.message);
+            DebugDump("Webmail.js : Exception in startUp " 
+                                            + e.name 
+                                            + ".\nError message: " 
+                                            + e.message);
         }
-        
-    	window.setTimeout(function()
-                          {
-                              gWebmailDebugLog.Write("Webmail: Webmail.js : WebmailStartUP - removeEventListener");
-                              window.removeEventListener("load",WebmailStartUp, false);
-                          },
-                          15);
-        
-    	gWebmailDebugLog.Write("Webmail: Webmail.js : WebmailStartUP - END ");
-    }
-    catch(e)
+    },
+    
+    shutDown : function ()
     {
-        DebugDump("Webmail: Webmail.js : Exception in WebmailStartUp " + e.name + ".\nError message: " + e.message);
-    }
-}
-
-
-
-function WebmailShutDown()
-{
-    gWebmailDebugLog.Write("Webmail : Webmail.js : WebmailShutDown - START");
-   
-    if  (gPOP) gPOP.Stop(); //stop pop server
-          
-    //account wizard
-    g_AccountWizard.deleteISP();
-       
-    gWebmailDebugLog.Write("Webmail : Webmail.js : WebmailShutDown - END");
-}
+        try
+        {
+            this.m_Log.Write("Webmail.js : shutDown - START");
+           
+            if  (this.m_POP) this.m_POP.Stop(); //stop pop server
+            if  (this.m_SMTP) this.m_SMTP.Stop(); //stop SMTP server
+                  
+            //account wizard
+            this.m_AccountWizard.deleteISP();
+               
+            this.m_Log..Write("Webmail.js : shutDown - END");
+        }
+        catch(e)
+        {
+            this.m_Log..DebugDump("Webmail.js : Exception in shutDown " 
+                                            + e.name 
+                                            + ".\nError message: " 
+                                            + e.message);
+        }
+    },
+};
