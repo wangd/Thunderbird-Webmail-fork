37c68b8f76f43c5223a16495c8036f78b8dbe6f2
archer <archer>
2005-06-24 22:45:16 +0000

    initial upload

---
+++ src/yahoo/components/nsYahooSMTP.js
@@ -0,0 +1,321 @@
+/*****************************  Globals   *************************************/                 
+const nsYahooSMTPClassID = Components.ID("{958266e0-e2a6-11d9-8cd6-0800200c9a66}");
+const nsYahooSMTPContactID = "@mozilla.org/YahooSMTP;1";
+const ExtYahooGuid = "{d7103710-6112-11d9-9669-0800200c9a66}";
+
+
+/******************************  Yahoo ***************************************/
+function nsYahooSMTP()
+{
+    try
+    {       
+        var scriptLoader =  Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+                              .getService(Components.interfaces.mozIJSSubScriptLoader);
+        
+        scriptLoader.loadSubScript("chrome://web-mail/content/common/DebugLog.js");
+        scriptLoader.loadSubScript("chrome://web-mail/content/common/CookieManager.js");
+        scriptLoader.loadSubScript("chrome://web-mail/content/common/CommonPrefs.js");
+       
+        
+        var date = new Date();
+        var  szLogFileName = "Yahoo SMTP Log - " + date.getHours()
+                                           + "-" + date.getMinutes() 
+                                           + "-"+ date.getUTCMilliseconds() +" -";
+        this.m_Log = new DebugLog("webmail.logging.comms", ExtYahooGuid, szLogFileName); 
+        
+        this.m_Log.Write("nsYahoo.js - Constructor - START");   
+       
+        this.m_bAuthorised = false;
+        this.m_szUserName = null;   
+        this.m_szPassWord = null; 
+        this.m_oResponseStream = null;  
+        this.m_szLocationURI = null;
+        this.m_oCookies = new CookieHandler(this.m_Log);    
+        this.m_iStage = 0;
+                       
+        this.m_Log.Write("nsYahoo.js - Constructor - END");  
+    }
+    catch(e)
+    {
+        DebugDump("nsYahoo.js: Constructor : Exception : " 
+                                      + e.name 
+                                      + ".\nError message: " 
+                                      + e.message);
+    }
+}
+
+
+
+nsYahooSMTP.prototype =
+{
+    get userName() {return this.m_szUserName;},
+    set userName(userName) {return this.m_szUserName = userName;},
+  
+    get passWord() {return this.m_szPassWord;},
+    set passWord(passWord) {return this.m_szPassWord = passWord;},
+    
+    get bAuthorised() {return this.m_bAuthorised;},
+  
+    get ResponseStream() {return this.m_oResponseStream;},
+    set ResponseStream(responseStream) {return this.m_oResponseStream = responseStream;},
+    
+    
+    logIn : function()
+    {
+        try
+        {
+            this.m_Log.Write("nsYahoo.js - logIN - START");   
+            this.m_Log.Write("nsYahoo.js - logIN - Username: " + this.m_szUserName 
+                                                   + " Password: " + this.m_szPassWord 
+                                                   + " stream: " + this.m_oResponseStream);
+           
+            this.serverComms("235 Authentication successful\r\n");
+            
+            this.m_Log.Write("nsYahoo.js - logIN - END");    
+            return true;
+        }
+        catch(e)
+        {
+            this.m_Log.DebugDump("nsYahoo.js: logIN : Exception : " 
+                                              + e.name + 
+                                              ".\nError message: " 
+                                              + e.message);
+            return false;
+        }
+    },
+
+   
+    rawMSG : function (szEmail)
+    {
+        try
+        {
+            this.m_Log.Write("nsYahoo.js - rawMSG - START");   
+            this.m_Log.Write("nsYahoo.js - rawMSG " + szEmail);
+           
+            
+            this.serverComms("250 OK\r\n");
+            
+            this.m_Log.Write("nsYahoo.js - rawMSG - END");    
+            return true;
+        }
+        catch(err)
+        {
+            this.m_Log.DebugDump("nsYahoo.js: rawMSG : Exception : " 
+                                              + e.name + 
+                                              ".\nError message: " 
+                                              + e.message);
+            return false;
+        }
+    },
+    
+    ////////////////////////////////////////////////////////////////////////////
+    /////  Comms                  
+    
+    serverComms : function (szMsg)
+    {
+        try
+        { 
+            this.m_Log.Write("nsYahoo.js - serverComms - START");
+            this.m_Log.Write("nsYahoo.js - serverComms msg " + szMsg);
+            var iCount = this.m_oResponseStream.write(szMsg,szMsg.length);
+            this.m_Log.Write("nsYahoo.js - serverComms sent count: " + iCount 
+                                                        +" msg length: " +szMsg.length);
+            this.m_Log.Write("nsYahoo.js - serverComms - END");  
+        }
+        catch(e)
+        {
+            this.m_Log.DebugDump("nsYahoo.js: serverComms : Exception : " 
+                                              + e.name 
+                                              + ".\nError message: " 
+                                              + e.message);
+        }
+    },
+   
+
+    
+    
+    httpConnection : function (szURL, szType, szData, szCookies ,callBack)
+    {
+        try
+        {
+            this.m_Log.Write("nsYahoo.js - httpConnection - START");   
+            this.m_Log.Write("nsYahoo.js - httpConnection - " + szURL + "\n"
+                                                              + szType + "\n"
+                                                              + szCookies + "\n"
+                                                              + szData );  
+            
+            
+            var ioService = Components.classes["@mozilla.org/network/io-service;1"].
+                                    getService(Components.interfaces.nsIIOService);
+      
+            var uri = ioService.newURI(szURL, null, null);
+            var channel = ioService.newChannelFromURI(uri);
+            var HttpRequest = channel.QueryInterface(Components.interfaces.nsIHttpChannel);                                     
+            HttpRequest.redirectionLimit = 0; //stops automatic redirect handling
+            
+            var component = this;             
+            
+              
+            //set cookies
+            if (szCookies)
+            {
+                this.m_Log.Write("nsYahoo.js - httpConnection - adding cookie \n"+ szCookies);
+                HttpRequest.setRequestHeader("x-CookieHack", "Hacker\r\nCookie: "  + szCookies , false);
+            }
+           
+           
+            //set data
+            if (szData)
+            {
+                this.m_Log.Write("nsYahoo.js - httpConnection - adding data");
+                
+                var uploadStream = Components.classes["@mozilla.org/io/string-input-stream;1"]
+                                    .createInstance(Components.interfaces.nsIStringInputStream);         
+                uploadStream.setData(szData, szData.length);
+        
+                var uploadChannel = channel.QueryInterface(Components.interfaces.nsIUploadChannel);
+                uploadChannel.setUploadStream(uploadStream, "application/x-www-form-urlencoded", -1); 
+            }
+            HttpRequest.requestMethod = szType;
+            
+           // HttpRequest.setRequestHeader("User-Agent", 
+           //               "Mozilla/5.0 (Windows; U; Windows NT 5.1;en-US; rv:1.7.5) Gecko/20041206 Thunderbird/1.0" ,
+           //               false);
+           // HttpRequest.setRequestHeader("Accept-Language", "en-US" , false);
+            
+            var listener = new this.downloadListener(callBack, this);
+            channel.asyncOpen(listener, null);  
+            
+            this.m_Log.Write("nsYahoo.js - httpConnection - END"); 
+            
+            return true;  
+        }
+        catch(e)
+        {
+            this.m_Log.DebugDump("nsYahoo.js: httpConnection : Exception : " 
+                                              + e.name 
+                                              + ".\nError message: " 
+                                              + e.message);
+            return false;
+        }
+    },
+    
+    
+    downloadListener : function(CallbackFunc, parent) 
+    {
+        return ({
+            m_data : "",
+            
+            onStartRequest : function (aRequest, aContext) 
+            {                 
+                this.m_data = "";
+            },
+            
+            
+            onDataAvailable : function (aRequest, aContext, aStream, aSourceOffset, aLength)
+            {               
+                var scriptableInputStream = Components.classes["@mozilla.org/scriptableinputstream;1"]
+                                 .createInstance(Components.interfaces.nsIScriptableInputStream);
+                scriptableInputStream.init(aStream);
+            
+                this.m_data += scriptableInputStream.read(aLength);
+            },
+            
+            
+            onStopRequest : function (aRequest, aContext, aStatus) 
+            {
+                CallbackFunc(this.m_data, aRequest, parent);
+            },
+            
+            
+            QueryInterface : function(aIID) 
+            {
+                if (aIID.equals(Components.interfaces.nsIStreamListener) ||
+                          aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
+                          aIID.equals(Components.interfaces.nsIAlertListener) ||
+                          aIID.equals(Components.interfaces.nsISupports))
+                    return this;
+                
+                throw Components.results.NS_NOINTERFACE;
+            }            
+        });
+    },
+        
+    
+    
+    
+/******************************************************************************/
+/***************** XPCOM  stuff ***********************************************/
+/******************************************************************************/
+    QueryInterface : function (iid)
+    {
+        if (!iid.equals(Components.interfaces.nsISMTPDomainHandler) 
+        	                && !iid.equals(Components.interfaces.nsISupports))
+            throw Components.results.NS_ERROR_NO_INTERFACE;
+            
+        return this;
+    }
+}
+ 
+
+/******************************************************************************/
+/* FACTORY*/
+var nsYahooSMTPFactory = new Object();
+
+nsYahooSMTPFactory.createInstance = function (outer, iid)
+{
+    if (outer != null) throw Components.results.NS_ERROR_NO_AGGREGATION;
+    
+    if (!iid.equals(nsYahooSMTPClassID) && !iid.equals(Components.interfaces.nsISupports))
+        throw Components.results.NS_ERROR_INVALID_ARG;
+    
+    return new nsYahooSMTP();
+}
+
+
+/******************************************************************************/
+/* MODULE */
+var nsYahooSMTPModule = new Object();
+
+nsYahooSMTPModule.registerSelf = function(compMgr, fileSpec, location, type)
+{
+    compMgr = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+    compMgr.registerFactoryLocation(nsYahooSMTPClassID,
+                                    "YahooSMTPComponent",
+                                    nsYahooSMTPContactID, 
+                                    fileSpec,
+                                    location, 
+                                    type);
+}
+
+
+nsYahooSMTPModule.unregisterSelf = function(aCompMgr, aFileSpec, aLocation)
+{
+    aCompMgr = aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+    aCompMgr.unregisterFactoryLocation(nsYahooSMTPClassID, aFileSpec);
+}
+
+ 
+nsYahooSMTPModule.getClassObject = function(compMgr, cid, iid)
+{
+    if (!cid.equals(nsYahooSMTPClassID))
+        throw Components.results.NS_ERROR_NO_INTERFACE;
+
+    if (!iid.equals(Components.interfaces.nsIFactory))
+        throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+
+    return nsYahooSMTPFactory;
+}
+
+
+nsYahooSMTPModule.canUnload = function(compMgr)
+{
+    return true;
+}
+/******************************************************************************/
+
+
+function NSGetModule(compMgr, fileSpec)
+{
+    return nsYahooSMTPModule; 
+}
