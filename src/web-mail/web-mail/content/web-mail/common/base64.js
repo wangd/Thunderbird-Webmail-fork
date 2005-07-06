e7d7ce8b37413b6d2425ffeacabd151d74b36002
archer <archer>
2005-07-06 23:04:06 +0000

    named spaced

--- src/web-mail/web-mail/content/web-mail/common/base64.js
+++ src/web-mail/web-mail/content/web-mail/common/base64.js
@@ -1,19 +1,23 @@
 // Base64
 // if encoding/decoding fails, return old content
-function EncBase64(asContent)
+function base64()
+{   
+    this.m_hShellService = Components.classes["@mozilla.org/appshell/appShellService;1"].
+                                     getService(Components.interfaces.nsIAppShellService);
+                                     
+    this.m_HiddenWindow = this.m_hShellService.hiddenDOMWindow;
+}
+
+
+
+base64.prototype.encode = function(szMsg)
 {
-    var hShellService = Components.classes["@mozilla.org/appshell/appShellService;1"].
-                                     getService(Components.interfaces.nsIAppShellService);
-    var hHiddenWindow = hShellService.hiddenDOMWindow;
-    return hHiddenWindow.btoa(asContent);
-  
+    return this.m_HiddenWindow.btoa(szMsg);
 }
 
-function DecBase64(asContent)
-{ 
-    var hShellService = Components.classes["@mozilla.org/appshell/appShellService;1"].
-                                     getService(Components.interfaces.nsIAppShellService);
-    var hHiddenWindow = hShellService.hiddenDOMWindow;
-    return hHiddenWindow.atob(asContent);
-}
 
+
+base64.prototype.decode = function(szMsg)
+{
+    return this.m_HiddenWindow.atob(szMsg);
+}
