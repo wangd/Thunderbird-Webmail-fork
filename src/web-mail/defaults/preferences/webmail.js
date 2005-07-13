d972f1dd9a7bfdbecc0dce1b3f3015f42d548b99
archer <archer>
2005-07-13 22:58:34 +0000

    added server switches

--- src/web-mail/defaults/preferences/webmail.js
+++ src/web-mail/defaults/preferences/webmail.js
@@ -6,4 +6,9 @@ pref("webmail.logging.comms.UseJSconsole", true);
 pref("webmail.logging.comms.UseFile", false);
 pref("webmail.logging.comms.FileLocation", "");
 pref("webmail.UseAccountWizard", true);
+pref("webmail.bUsePOPServer", true);
+pref("webmail.bUseSMTPServer", false);
+pref("webmail.bUseIMAPServer", false);
+
+
 
