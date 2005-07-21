dc1ef079380a584a35823181d10d2ac667d7f5f1
archer <archer>
2005-07-21 13:09:27 +0000

    intial upload This is dummy object, it does nothing

---
+++ src/web-mail/web-mail/content/web-mail/common/Quoted-Printable.js
@@ -0,0 +1,15 @@
+function quoted-printable()
+{
+}
+
+
+quoted-printable.prototype.encode = function(msg)
+{
+    return msg;
+}
+
+quoted-printable.prototype.decode = function(msg)
+{
+    return msg;
+}
+
