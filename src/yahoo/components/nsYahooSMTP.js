29ee56622b16a8d59e4a4e2f75d86568ae02eef4
archer <archer>
2005-07-23 00:39:14 +0000

    fixed typo

--- src/yahoo/components/nsYahooSMTP.js
+++ src/yahoo/components/nsYahooSMTP.js
@@ -697,10 +697,10 @@ nsYahooSMTP.prototype =
                                 szBody = oBase64.decode(szBody.replace(/\r\n/gm,""));
                                 mainObject.m_HttpComms.addFormData(szName,szBody,true,szFileName,true); 
                             } 
-                            eles if (szEncoding.search(/quoted-printable/i)!=-1)
+                            else if (szEncoding.search(/quoted-printable/i)!=-1)
                             {
                                 mainObject.m_Log.Write("nsYahooSMTP.js - composerOnloadHandler - encoded QP");  
-                                var oQP = new quoted-printable()
+                                var oQP = new QuotedPrintable();
                                 szBody = oQP.decode(szBody);
                                 mainObject.m_HttpComms.addFormData(szName,szBody,true,szFileName,false);    
                             }
