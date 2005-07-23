bcd993885a2f8f0ed6673b1d2d385e8c48927768
archer <archer>
2005-07-23 00:18:55 +0000

    fixed object name

--- src/web-mail/web-mail/content/web-mail/common/Quoted-Printable.js
+++ src/web-mail/web-mail/content/web-mail/common/Quoted-Printable.js
@@ -1,14 +1,14 @@
-function quoted-printable()
+function QuotedPrintable()
 {
 }
 
 
-quoted-printable.prototype.encode = function(msg)
+QuotedPrintable.prototype.encode = function(msg)
 {
     return msg;
 }
 
-quoted-printable.prototype.decode = function(msg)
+QuotedPrintable.prototype.decode = function(msg)
 {
     return msg;
 }
