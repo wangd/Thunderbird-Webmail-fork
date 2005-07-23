5b1617548ba0ac2969720c67a2ce59e18ec5f549
archer <archer>
2005-07-23 22:24:50 +0000

    intial upload

---
+++ src/yahoo/yahoo/content/yahoo/YahooMSG.js
@@ -0,0 +1,22 @@
+function YahooMSG()
+{
+}
+
+
+YahooMSG.prototype.szMSGUri = null;
+
+YahooMSG.prototype.bTrashFolder = false;
+
+YahooMSG.prototype.aData = null;
+
+YahooMSG.prototype.szDeleteUri = null;
+
+
+
+function YahooData()
+{
+}
+
+YahooData.prototype.szValue = null;
+
+YahooData.prototype.szName = null;
