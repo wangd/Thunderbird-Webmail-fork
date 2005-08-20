2f3b36f6e9a403f8e6f7df4773996bc6efb31262
archer <archer>
2005-08-20 00:32:01 +0000

    *** empty log message ***

--- src/lycos/defaults/preferences/lycos.js
+++ src/lycos/defaults/preferences/lycos.js
@@ -1,3 +1,3 @@
 pref("lycos.bUseJunkMail", false);
-pref("lycos.bSaveCopy", false);
+pref("lycos.bSaveCopy", true);
 pref("extensions.{10e6e940-8a9c-11d9-9669-0800200c9a66}.description", "chrome://lycos/locale/Lycos.properties");
