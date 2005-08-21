c82d5bef32cc07a89e40778b4999dcd80854f2c8
archer <archer>
2005-08-21 22:58:38 +0000

    added save copy

--- src/hotmail/defaults/preferences/hotmail.js
+++ src/hotmail/defaults/preferences/hotmail.js
@@ -1,3 +1,4 @@
 pref("hotmail.bUseJunkMail", false);
 pref("hotmail.webdav.iAccountNum", 0);
 pref("extensions.{a6a33690-2c6a-11d9-9669-0800200c9a66}.description", "chrome://hotmail/locale/Hotmail.properties");
+pref("hotmail.bSaveCopy", true);
