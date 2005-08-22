9f619db4c863d0b05db714f464e1f6d6cb2c7203
archer <archer>
2005-08-22 22:17:37 +0000

    intial upload

---
+++ src/hotmail/components/nsHotmailSMTP.js
@@ -0,0 +1,265 @@
+/*****************************  Globals   *************************************/                 
+const nsHotmailSMTPClassID = Components.ID("{8b9baa40-1296-11da-8cd6-0800200c9a66}");
+const nsHotmailSMTPContactID = "@mozilla.org/HotmailSMTP;1";
+const ExtHotmailGuid = "{3c8e8390-2cf6-11d9-9669-0800200c9a66}";
+
+/************************Screen Rippper constants******************************/
+const patternHotmailSMTPSRuri =/[^\.\/]+\.[^\.\/]+$/;
+
+
+/************************Webdav constants *************************************/
+const patternHotmailPOPWDuri = /.*?\.(.*?)$/;
+const HotmailSMTPSchema = "<?xml version=\"1.0\"?>\r\n<D:propfind xmlns:D=\"DAV:\" xmlns:h=\"http://schemas.microsoft.com/hotmail/\" xmlns:hm=\"urn:schemas:httpmail:\">\r\n<D:prop>\r\n<h:adbar/>\r\n<hm:contacts/>\r\n<hm:inbox/>\r\n<hm:outbox/>\r\n<hm:sendmsg/>\r\n<hm:sentitems/>\r\n<hm:deleteditems/>\r\n<hm:drafts/>\r\n<hm:msgfolderroot/>\r\n<h:maxpoll/>\r\n<h:sig/>\r\n</D:prop>\r\n</D:propfind>";
+const HotmailSMTPFolderSchema = "<?xml version=\"1.0\"?>\r\n<D:propfind xmlns:D=\"DAV:\" xmlns:hm=\"urn:schemas:httpmail:\">\r\n<D:prop>\r\n<D:isfolder/>\r\n<D:displayname/>\r\n<hm:special/>\r\n<D:hassubs/>\r\n<D:nosubs/>\r\n<hm:unreadcount/>\r\n<D:visiblecount/>\r\n<hm:special/>\r\n</D:prop>\r\n</D:propfind>";
+const HotmailSMTPMailSchema = "<?xml version=\"1.0\"?>\r\n<D:propfind xmlns:D=\"DAV:\" xmlns:hm=\"urn:schemas:httpmail:\" xmlns:m=\"urn:schemas:mailheader:\">\r\n<D:prop>\r\n<D:isfolder/>\r\n<hm:read/>\r\n<m:hasattachment/>\r\n<m:to/>\r\n<m:from/>\r\n<m:subject/>\r\n<m:date/>\r\n<D:getcontentlength/>\r\n</D:prop>\r\n</D:propfind>";
+const HotmailSendMsgPattern = /<hm:sendmsg>(.*?)<\/hm:sendmsg>/;
+
+
+/******************************  Hotmail ***************************************/
+function nsHotmailSMTP()
+{
+    try
+    {       
+        var scriptLoader =  Components.classes["@mozilla.org/moz/jssubscript-loader;1"];
+        scriptLoader = scriptLoader.getService(Components.interfaces.mozIJSSubScriptLoader);
+        scriptLoader.loadSubScript("chrome://web-mail/content/common/DebugLog.js");
+        scriptLoader.loadSubScript("chrome://web-mail/content/common/CommonPrefs.js");
+        scriptLoader.loadSubScript("chrome://hotmail/content/Hotmail-WebDav-SMTP.js");
+        scriptLoader.loadSubScript("chrome://hotmail/content/Hotmail-ScreenRipper-SMTP.js");
+              
+        
+        var date = new Date();
+        var  szLogFileName = "Hotmail SMTP Log - " + date.getHours()
+                                           + "-" + date.getMinutes() 
+                                           + "-"+ date.getUTCMilliseconds() +" -";
+        this.m_Log = new DebugLog("webmail.logging.comms", ExtHotmailGuid, szLogFileName); 
+        
+        this.m_Log.Write("nsHotmailSMTP.js - Constructor - START");   
+       
+        this.m_szUserName = null;   
+        this.m_szPassWord = null; 
+        this.m_oResponseStream = null;  
+        this.m_aszTo = new Array;
+        this.m_szFrom = null;
+        this.m_CommMethod = null;
+              
+        //do i save copy
+        var oPref = new Object();
+        oPref.Value = null;
+        var  PrefAccess = new WebMailCommonPrefAccess();
+        if (PrefAccess.Get("bool","hotmail.bSaveCopy",oPref))
+            this.m_bSaveCopy=oPref.Value;
+        else
+            this.m_bSaveCopy=true;          
+        delete oPref;
+        
+        
+        this.m_Log.Write("nsHotmailSMTP.js - Constructor - END");  
+    }
+    catch(err)
+    {
+        DebugDump("nsHotmailSMTP.js: Constructor : Exception : " 
+                                      + err.name 
+                                      + ".\nError message: " 
+                                      + err.message +"\n" +
+                                      err.lineNumber);
+    }
+}
+
+
+
+nsHotmailSMTP.prototype =
+{
+    get userName() {return this.m_szUserName;},
+    set userName(userName) {return this.m_szUserName = userName;},
+  
+    get passWord() {return this.m_szPassWord;},
+    set passWord(passWord) {return this.m_szPassWord = passWord;},
+    
+    get ResponseStream() {return this.m_oResponseStream;},
+    set ResponseStream(responseStream) {return this.m_oResponseStream = responseStream;},
+    
+    get to() {return this.m_aszTo;},
+    set to(szAddress) {return this.m_aszTo.push(szAddress);},
+    
+    get from() {return this.m_szFrom;},
+    set from(szAddress) {return this.m_szFrom = szAddress;},
+    
+    get bAuthorised() 
+    {
+        return (this.m_CommMethod)? this.m_CommMethod.m_bAuthorised: false;
+    },
+  
+    
+    
+    
+    logIn : function()
+    {
+        try
+        {
+            this.m_Log.Write("nsHotmailSMTP.js - logIN - START");   
+            this.m_Log.Write("nsHotmailSMTP.js - logIN - Username: " + this.m_szUserName 
+                                                   + " Password: " + this.m_szPassWord 
+                                                   + " stream: " + this.m_oResponseStream);
+           
+            if (!this.m_szUserName || !this.m_oResponseStream  || !this.m_szPassWord) return false;
+             
+            //load webdav address
+            var iAccountNum=0;
+            var WebMailPrefAccess = new WebMailCommonPrefAccess();
+            var oPref = new Object();
+            oPref.Value = null;
+            
+            if (WebMailPrefAccess.Get("int","hotmail.webdav.iAccountNum",oPref))
+                iAccountNum = oPref.Value;
+            
+            if (iAccountNum>0)
+            {
+                this.m_Log.Write("nsHotmailSMTP.js - logIN - Accountnum " + iAccountNum);
+                for (i=0; i<iAccountNum; i++)
+                {
+                    this.m_Log.Write("nsHotmailSMTP.js - logIN - username search " + i);
+                    WebMailPrefAccess.Get("char","hotmail.webdav.Account."+i,oPref);
+                    var reg = new RegExp(oPref.Value,"i");
+                    this.m_Log.Write("nsHotmailSMTP.js - logIN - username search " + reg);
+                    if (this.m_szUserName.match(reg))
+                    {
+                        this.m_Log.Write("nsHotmailSMTP.js - logIN - username found");  
+                        this.m_CommMethod = new HotmailSMTPWebDav(this.m_oResponseStream, 
+                                                                  this.m_Log, 
+                                                                  this.m_bSaveCopy);    
+                    } 
+                }
+            }
+           
+            if (!this.m_CommMethod)
+                this.m_CommMethod = new HotmailSMTPScreenRipper(this.m_oResponseStream, 
+                                                                this.m_Log, 
+                                                                this.m_bSaveCopy);
+             
+            var bResult = this.m_CommMethod.logIn(this.m_szUserName,   
+                                                  this.m_szPassWord);
+                       
+            this.m_Log.Write("nsHotmailSMTP.js - logIN - "+ bResult +"- END");    
+            return bResult;
+        }
+        catch(err)
+        {
+            this.m_Log.DebugDump("nsHotmailSMTP.js: logIN : Exception : " 
+                                              + err.name + 
+                                              ".\nError message: " 
+                                              + err.message+ "\n"
+                                              + err.lineNumber);
+                                              
+            this.serverComms("502 negative vibes\r\n");
+            return false;
+        }
+    },
+
+       
+    
+    rawMSG : function (szEmail)
+    {
+        try
+        {
+            this.m_Log.Write("nsHotmailSMTP.js - rawMSG - START");   
+            this.m_Log.Write("nsHotmailSMTP.js - rawMSG " + szEmail);
+    
+            var bResult = this.m_CommMethod.rawMSG(this.m_szFrom, 
+                                                   this.m_aszTo,
+                                                   szEmail);
+            
+            this.m_Log.Write("nsHotmailSMTP.js - rawMSG -" + bResult +" END");    
+            return bResult;
+        }
+        catch(err)
+        {
+            this.m_Log.DebugDump("nsHotmailSMTP.js: rawMSG : Exception : " 
+                                              + err.name + 
+                                              ".\nError message: " 
+                                              + err.message+ "\n"
+                                              + err.lineNumber);
+                                              
+            this.serverComms("502 negative vibes\r\n");
+            return false;
+        }
+    },
+    
+    
+   
+     
+/******************************************************************************/
+/***************** XPCOM  stuff ***********************************************/
+/******************************************************************************/
+    QueryInterface : function (iid)
+    {
+        if (!iid.equals(Components.interfaces.nsISMTPDomainHandler) 
+        	                && !iid.equals(Components.interfaces.nsISupports))
+            throw Components.results.NS_ERROR_NO_INTERFACE;
+            
+        return this;
+    }
+}
+ 
+
+/******************************************************************************/
+/* FACTORY*/
+var nsHotmailSMTPFactory = new Object();
+
+nsHotmailSMTPFactory.createInstance = function (outer, iid)
+{
+    if (outer != null) throw Components.results.NS_ERROR_NO_AGGREGATION;
+    
+    if (!iid.equals(nsHotmailSMTPClassID) && !iid.equals(Components.interfaces.nsISupports))
+        throw Components.results.NS_ERROR_INVALID_ARG;
+    
+    return new nsHotmailSMTP();
+}
+
+
+/******************************************************************************/
+/* MODULE */
+var nsHotmailSMTPModule = new Object();
+
+nsHotmailSMTPModule.registerSelf = function(compMgr, fileSpec, location, type)
+{
+    compMgr = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+    compMgr.registerFactoryLocation(nsHotmailSMTPClassID,
+                                    "HotmailSMTPComponent",
+                                    nsHotmailSMTPContactID, 
+                                    fileSpec,
+                                    location, 
+                                    type);
+}
+
+
+nsHotmailSMTPModule.unregisterSelf = function(aCompMgr, aFileSpec, aLocation)
+{
+    aCompMgr = aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+    aCompMgr.unregisterFactoryLocation(nsHotmailSMTPClassID, aFileSpec);
+}
+
+ 
+nsHotmailSMTPModule.getClassObject = function(compMgr, cid, iid)
+{
+    if (!cid.equals(nsHotmailSMTPClassID))
+        throw Components.results.NS_ERROR_NO_INTERFACE;
+
+    if (!iid.equals(Components.interfaces.nsIFactory))
+        throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+
+    return nsHotmailSMTPFactory;
+}
+
+
+nsHotmailSMTPModule.canUnload = function(compMgr)
+{
+    return true;
+}
+/******************************************************************************/
+
+
+function NSGetModule(compMgr, fileSpec)
+{
+    return nsHotmailSMTPModule; 
+}
---
+++ src/hotmail/hotmail/content/hotmail/Hotmail-WebDav-SMTP.js
@@ -0,0 +1,307 @@
+function HotmailSMTPWebDav(oResponseStream, oLog, bSaveCopy)
+{
+    try
+    {       
+        var scriptLoader =  Components.classes["@mozilla.org/moz/jssubscript-loader;1"];
+        scriptLoader = scriptLoader.getService(Components.interfaces.mozIJSSubScriptLoader);
+        scriptLoader.loadSubScript("chrome://web-mail/content/common/DebugLog.js");
+        scriptLoader.loadSubScript("chrome://hotmail/content/Hotmail-AuthTokenManager.js");
+        scriptLoader.loadSubScript("chrome://web-mail/content/common/comms.js");
+                    
+        this.m_Log = oLog; 
+        this.m_Log.Write("HotmailWebDav.js - Constructor - START");   
+        
+        this.m_bAuthorised = false;        
+        this.m_szUserName = null;
+        this.m_szPassWord = null;
+        this.m_oResponseStream = oResponseStream;       
+        this.m_HttpComms = new Comms(this,this.m_Log); 
+        this.m_HttpComms.setHandleBounce(false);
+        this.m_AuthToken = new AuthTokenHandler(this.m_Log);
+        this.m_bSaveCopy =  bSaveCopy;
+        
+        this.m_iStage=0; 
+        this.m_szSendUri = null;
+       
+        this.m_IOS = Components.classes["@mozilla.org/network/io-service;1"];
+        this.m_IOS = this.m_IOS.getService(Components.interfaces.nsIIOService);
+        
+        this.m_bJunkMail = false;
+        
+        this.m_Log.Write("HotmailWD-SMTP.js - Constructor - END");  
+    }
+    catch(e)
+    {
+        DebugDump("HotmailWD-SMTP.js: Constructor : Exception : " 
+                                      + e.name 
+                                      + " line " 
+                                      + e.linenumber
+                                      + ".\nError message: " 
+                                      + e.message);
+    }
+}
+
+
+HotmailSMTPWebDav.prototype =
+{
+   
+    logIn : function(szUserName, szPassWord)
+    {
+        try
+        {
+            this.m_Log.Write("HotmailWD-SMTP.js - logIN - START");   
+            this.m_Log.Write("HotmailWD-SMTP.js - logIN - Username: " + szUserName 
+                                                   + " Password: " + szPassWord 
+                                                   + " stream: " + this.m_oResponseStream);
+            this.m_szUserName = szUserName;
+            this.m_szPassWord = szPassWord;
+            
+            if (!this.m_szUserName || !this.m_oResponseStream || !this.m_szPassWord) return false;
+            
+            this.m_iStage= 0;
+            this.m_HttpComms.clean();
+            this.m_HttpComms.setContentType(-1);
+            this.m_HttpComms.setURI("http://services.msn.com/svcs/hotmail/httpmail.asp");
+            this.m_HttpComms.setRequestMethod("PROPFIND");
+            this.m_HttpComms.addData(HotmailSMTPSchema,"text/xml");
+            var bResult = this.m_HttpComms.send(this.loginOnloadHandler);                             
+            if (!bResult) throw new Error("httpConnection returned false");
+                        
+            this.m_Log.Write("HotmailWD-SMTP.js - logIN - END");    
+            return bResult;
+        }
+        catch(e)
+        {
+            this.m_Log.DebugDump("HotmailWD-SMTP.js: logIN : Exception : " 
+                                              + e.name + 
+                                              ".\nError message: " 
+                                              + e.message+ "\n"
+                                              + e.lineNumber);
+            return false;
+        }
+    },
+
+
+    loginOnloadHandler : function(szResponse ,event , mainObject)
+    {
+        try
+        {
+            mainObject.m_Log.Write("HotmailWD-SMTP.js - loginOnloadHandler - START"); 
+            mainObject.m_Log.Write("HotmailWD-SMTP.js - loginOnloadHandler : " + mainObject.m_iStage);  
+            
+            var httpChannel = event.QueryInterface(Components.interfaces.nsIHttpChannel);
+            
+            //if this fails we've gone somewhere new
+            mainObject.m_Log.Write("HotmailWD-SMTP.js - loginOnloadHandler - status :" +httpChannel.responseStatus );
+           
+            if (httpChannel.responseStatus != 200 && 
+                    httpChannel.responseStatus != 207 &&
+                        httpChannel.responseStatus != 302  &&
+                            httpChannel.responseStatus != 401) 
+                throw new Error("return status " + httpChannel.responseStatus);
+            
+            mainObject.m_HttpComms.clean();
+            
+            //bounce handler
+            if ( httpChannel.responseStatus == 302)
+            {
+                var szLocation = null;
+                try
+                {
+                    szLocation =  httpChannel.getResponseHeader("Location");
+                    mainObject.m_Log.Write("HotmailWD-SMTP.js - loginOnloadHandler - location \n" + szLocation);  
+                }
+                catch(e)
+                {
+                    throw new Error("Location header not found")
+                } 
+            
+                mainObject.m_HttpComms.setContentType(-1);
+                mainObject.m_HttpComms.setURI(szLocation);
+                mainObject.m_HttpComms.setRequestMethod("PROPFIND");
+                mainObject.m_HttpComms.addData(HotmailSMTPSchema,"text/xml");
+                var bResult = mainObject.m_HttpComms.send(mainObject.loginOnloadHandler);                             
+                if (!bResult) throw new Error("httpConnection returned false");
+            }
+            //Authenticate
+            else if  (httpChannel.responseStatus == 401)
+            {
+                mainObject.m_iStage++;
+                var szURL = mainObject.m_IOS.newURI(httpChannel.URI.spec,null,null).prePath;
+                var aszHost = szURL.match(patternHotmailSMTPSRuri); 
+                
+                try
+                {
+                    var szAuthenticate =  httpChannel.getResponseHeader("www-Authenticate");
+                    mainObject.m_Log.Write("HotmailWD-SMTP.js - loginOnloadHandler - www-Authenticate " + szAuthenticate);                      
+                }
+                catch(err)
+                {
+                     mainObject.m_Log.DebugDump("HotmailWD-SMTP.js: loginHandler  Authenitcation: Exception : " 
+                                                      + err.name 
+                                                      + ".\nError message: " 
+                                                      + err.message+ "\n"
+                                                      + err.lineNumber);
+                                                      
+                    throw new Error("szAuthenticate header not found")
+                }     
+                    
+                //basic or digest
+                if (szAuthenticate.search(/basic/i)!= -1)
+                {//authentication on the cheap
+                    throw new Error("unspported authentication method");
+                }
+                else if (szAuthenticate.search(/digest/i)!= -1)
+                {   
+                    //get realm
+                    var szRealm = szAuthenticate.match(/realm="(.*?)"/)[1];
+                    mainObject.m_AuthToken.addToken(szRealm, 
+                                                    szAuthenticate , 
+                                                    httpChannel.URI.path ,
+                                                    mainObject.m_szUserName, 
+                                                    mainObject.m_szPassWord);
+                                                    
+                    var szAuthString = mainObject.m_AuthToken.findToken(szRealm);
+                    
+                    mainObject.m_HttpComms.setContentType(-1);
+                    mainObject.m_HttpComms.setURI(httpChannel.URI.spec);
+                    mainObject.m_HttpComms.addRequestHeader("Authorization", szAuthString , false);
+                    mainObject.m_HttpComms.setRequestMethod("PROPFIND");
+                    mainObject.m_HttpComms.addData(HotmailSMTPSchema,"text/xml");
+                    var bResult = mainObject.m_HttpComms.send(mainObject.loginOnloadHandler);                             
+                    if (!bResult) throw new Error("httpConnection returned false");
+               }
+                else
+                    throw new Error("unknown authentication method");
+            } 
+            else //everything else
+            {
+                mainObject.m_iStage++;
+                mainObject.m_Log.Write("HotmailWD-SMTP.js - loginOnloadHandler - get url - start");
+                mainObject.m_iAuth=0; //reset login counter
+               
+                mainObject.m_szSendUri = szResponse.match(HotmailSendMsgPattern)[1];
+                mainObject.m_Log.Write("HotmailWD-SMTP.js - loginOnloadHandler - Send URi - " +mainObject.m_szSendUri);
+                
+                //server response
+                mainObject.serverComms("235 Your In\r\n");
+                mainObject.m_bAuthorised = true;
+                        
+                mainObject.m_Log.Write("HotmailWD-SMTP.js - loginOnloadHandler - get url - end"); 
+            }
+            mainObject.m_Log.Write("HotmailWD-SMTP.js - loginOnloadHandler - END");
+        }
+        catch(err)
+        {
+            mainObject.m_Log.DebugDump("HotmailWD-SMTP.js: loginHandler : Exception : " 
+                                          + err.name 
+                                          + ".\nError message: " 
+                                          + err.message+ "\n"
+                                          + err.lineNumber);
+            
+            mainObject.serverComms("-ERR webdav error\r\n");
+        }
+    },
+    
+    
+        
+    rawMSG : function ( szFrom, aszTo, szEmail)
+    {
+        try
+        {
+            this.m_Log.Write("HotmailWD-SMTP.js - rawMSG - START");   
+                
+            this.m_iStage = 0;
+            szEmail = szEmail.replace(/^\.$/m, ""); //removes SMTP termiator
+            //get composer page
+            var szMsg = "MAIL FROM:<"+szFrom+">\r\n";
+            for (i=0; i< aszTo.length; i++)
+            {
+                szMsg +="RCPT TO:<"+aszTo[i]+">\r\n";
+            }
+            szMsg +="\r\n";
+            szMsg += szEmail;
+            szMsg +="\r\n\r\n";
+            
+            this.m_HttpComms.clean();
+            this.m_HttpComms.setContentType(-1);
+            this.m_HttpComms.setURI(this.m_szSendUri);
+            this.m_HttpComms.setRequestMethod("POST");
+            this.m_HttpComms.addRequestHeader("SAVEINSENT", this.m_bSaveCopy?"t":"f", false); 
+            this.m_HttpComms.addData(szMsg,"message/rfc821");     
+            this.m_HttpComms.addRequestHeader("Authorization", this.m_szAuthString , false);  
+            var bResult = this.m_HttpComms.send(this.composerOnloadHandler);  
+            if (!bResult) throw new Error("httpConnection returned false");
+            
+            this.m_Log.Write("HotmailWD-SMTP.js - rawMSG - END");    
+            return true;
+        }
+        catch(err)
+        {
+            this.m_Log.DebugDump("HotmailWD-SMTP.js: rawMSG : Exception : " 
+                                              + err.name + 
+                                              ".\nError message: " 
+                                              + err.message +"\n" +
+                                                err.lineNumber);
+                                                
+            this.serverComms("502 negative vibes\r\n");
+            
+            return false;
+        }
+    },
+     
+     
+    
+    
+    composerOnloadHandler : function(szResponse ,event , mainObject)
+    {
+        try
+        {
+            mainObject.m_Log.Write("HotmailWD-SMTP.js - composerOnloadHandler - START"); 
+            mainObject.m_Log.Write("HotmailWD-SMTP.js - composerOnloadHandler : " + mainObject.m_iStage);  
+            
+            var httpChannel = event.QueryInterface(Components.interfaces.nsIHttpChannel);
+            
+            //if this fails we've gone somewhere new
+            mainObject.m_Log.Write("HotmailWD-SMTP.js - composerOnloadHandler - status :" +httpChannel.responseStatus );
+            if (httpChannel.responseStatus <=199 || httpChannel.responseStatus >=300) 
+                mainObject.serverComms("502 Error Sending Email\r\n");  
+             
+            mainObject.serverComms("250 OK\r\n");       
+            mainObject.m_Log.Write("HotmailWD-SMTP.js - composerOnloadHandler - END");
+        }
+        catch(err)
+        {
+            mainObject.m_Log.DebugDump("HotmailWD-SMTP.js: composerOnloadHandler : Exception : " 
+                                          + err.name 
+                                          + ".\nError message: " 
+                                          + err.message +"\n" +
+                                            err.lineNumber);
+                                            
+            mainObject.serverComms("502 negative vibes\r\n");
+        }
+    },
+    
+     
+     
+    serverComms : function (szMsg)
+    {
+        try
+        { 
+            this.m_Log.Write("HotmailWD-SMTP.js - serverComms - START");
+            this.m_Log.Write("HotmailWD-SMTP.js - serverComms msg " + szMsg);
+            var iCount = this.m_oResponseStream.write(szMsg,szMsg.length);
+            this.m_Log.Write("HotmailWD-SMTP.js - serverComms sent count: " + iCount 
+                                                        +" msg length: " +szMsg.length);
+            this.m_Log.Write("HotmailWD-SMTP.js - serverComms - END");  
+        }
+        catch(e)
+        {
+            this.m_Log.DebugDump("HotmailWD-SMTP.js: serverComms : Exception : " 
+                                              + e.name 
+                                              + ".\nError message: " 
+                                              + e.message+ "\n"
+                                              + e.lineNumber);
+        }
+    },   
+}
