fa52581c84680e73a6e63d543b5bbccd4956aab6
archer <archer>
2005-09-13 23:35:35 +0000

    added to, from, subject, date headers

--- src/lycos/lycos/content/lycos/Lycos-POPMSG.js
+++ src/lycos/lycos/content/lycos/Lycos-POPMSG.js
@@ -7,3 +7,11 @@ LycosPOPMSG.prototype.szMSGUri = null;
 LycosPOPMSG.prototype.bJunkFolder = false;
 
 LycosPOPMSG.prototype.iSize = 0;
+
+LycosPOPMSG.prototype.szTo = null;
+
+LycosPOPMSG.prototype.szFrom = null;
+
+LycosPOPMSG.prototype.szSubject = null;
+
+LycosPOPMSG.prototype.szDate = null;
