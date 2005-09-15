472ba6cbc9c079d1878d08b1ce23c39be4919c78
archer <archer>
2005-09-15 00:11:48 +0000

    added download unread emails

--- src/maildotcom/defaults/preferences/MailDotCom.js
+++ src/maildotcom/defaults/preferences/MailDotCom.js
@@ -2,3 +2,4 @@ pref("maildotcom.bEmptyTrash", false);
 pref("extensions.{1ad5b3b0-b908-11d9-9669-0800200c9a66}.description", "chrome://maildotcom/locale/MailDotCom.properties");
 pref("maildotcom.bSaveCopy", false);
 pref("maildotcom.bSendHtml", false);
+pref("maildotcom.bDownloadUnread", false);
--- src/yahoo/defaults/preferences/yahoo.js
+++ src/yahoo/defaults/preferences/yahoo.js
@@ -2,4 +2,4 @@ pref("yahoo.bUseJunkMail", false);
 pref("yahoo.bSaveCopy", true);
 pref("yahoo.bSendHtml", true);
 pref("extensions.{d7103710-6112-11d9-9669-0800200c9a66}.description", "chrome://yahoo/locale/Yahoo.properties");
-
+pref("yahoo.bDownloadUnread", false);
