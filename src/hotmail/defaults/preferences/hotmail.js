8de2112cdb316de063c0904303e4df705e052558
archer <archer>
2005-09-16 00:52:03 +0000

    added download un read

--- src/hotmail/defaults/preferences/hotmail.js
+++ src/hotmail/defaults/preferences/hotmail.js
@@ -2,3 +2,4 @@ pref("hotmail.bUseJunkMail", false);
 pref("hotmail.webdav.iAccountNum", 0);
 pref("extensions.{a6a33690-2c6a-11d9-9669-0800200c9a66}.description", "chrome://hotmail/locale/Hotmail.properties");
 pref("hotmail.bSaveCopy", true);
+pref("hotmail.bDownloadUnread", false);
