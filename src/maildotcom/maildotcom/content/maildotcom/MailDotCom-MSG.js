912234aedeed3b36298120e604e28ddc07c58aa2
archer <archer>
2005-09-17 01:03:11 +0000

    new members

--- src/maildotcom/maildotcom/content/maildotcom/MailDotCom-MSG.js
+++ src/maildotcom/maildotcom/content/maildotcom/MailDotCom-MSG.js
@@ -7,3 +7,11 @@ MailDotComMSG.prototype.szMSGUri = null;
 MailDotComMSG.prototype.bJunkFolder = false;
 
 MailDotComMSG.prototype.iSize = 0;
+
+MailDotComMSG.prototype.szTo = null;
+
+MailDotComMSG.prototype.szFrom = null;
+
+MailDotComMSG.prototype.szSubject = null;
+
+MailDotComMSG.prototype.szDate = null;
