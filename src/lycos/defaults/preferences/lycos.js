377f6b1c313eae82e316ce5c274040a9beddbb8a
archer <archer>
2005-10-21 23:46:39 +0000

    added bReUseSession

--- src/lycos/defaults/preferences/lycos.js
+++ src/lycos/defaults/preferences/lycos.js
@@ -3,3 +3,4 @@ pref("lycos.bSaveCopy", true);
 pref("extensions.{10e6e940-8a9c-11d9-9669-0800200c9a66}.description", "chrome://lycos/locale/Lycos.properties");
 pref("lycos.bEmptyTrash", false);
 pref("lycos.bDownloadUnread", false);
+pref("lycos.bReUseSession", true);
--- src/maildotcom/defaults/preferences/MailDotCom.js
+++ src/maildotcom/defaults/preferences/MailDotCom.js
@@ -3,3 +3,4 @@ pref("extensions.{1ad5b3b0-b908-11d9-9669-0800200c9a66}.description", "chrome://
 pref("maildotcom.bSaveCopy", false);
 pref("maildotcom.bSendHtml", false);
 pref("maildotcom.bDownloadUnread", false);
+pref("maildotcom.bReUseSession", true);
