d3633e9e5d69b3e0ab7a8aef739787192de19331
archer <archer>
2005-10-21 19:24:04 +0000

    added bReUseSession

--- src/hotmail/defaults/preferences/hotmail.js
+++ src/hotmail/defaults/preferences/hotmail.js
@@ -3,3 +3,4 @@ pref("hotmail.webdav.iAccountNum", 0);
 pref("extensions.{a6a33690-2c6a-11d9-9669-0800200c9a66}.description", "chrome://hotmail/locale/Hotmail.properties");
 pref("hotmail.bSaveCopy", true);
 pref("hotmail.bDownloadUnread", false);
+pref("hotmail.bReUseSession", true);
