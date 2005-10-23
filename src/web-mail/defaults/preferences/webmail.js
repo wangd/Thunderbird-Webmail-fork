a9e1e7dc3ae5f419122fc797e9c9a5191c05b373
archer <archer>
2005-10-23 00:48:40 +0000

    smtp is now enabled by default

--- src/web-mail/defaults/preferences/webmail.js
+++ src/web-mail/defaults/preferences/webmail.js
@@ -7,7 +7,7 @@ pref("webmail.logging.comms.UseFile", false);
 pref("webmail.logging.comms.FileLocation", "");
 pref("webmail.UseAccountWizard", true);
 pref("webmail.bUsePOPServer", true);
-pref("webmail.bUseSMTPServer", false);
+pref("webmail.bUseSMTPServer", true);
 pref("webmail.bUseIMAPServer", false);
 pref("extensions.{3c8e8390-2cf6-11d9-9669-0800200c9a66}.description", "chrome://web-mail/locale/Webmail.properties");
 
