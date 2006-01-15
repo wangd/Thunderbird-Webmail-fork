7e00df0838afc1f94b5ada734e8fe5a57c061243
archer <archer>
2006-01-15 18:48:02 +0000

    added new prefs for mailbox split

--- src/hotmail/defaults/preferences/hotmail.js
+++ src/hotmail/defaults/preferences/hotmail.js
@@ -3,4 +3,5 @@ pref("extensions.{a6a33690-2c6a-11d9-9669-0800200c9a66}.description", "chrome://
 pref("hotmail.bSaveCopy", true);
 pref("hotmail.bDownloadUnread", false);
 pref("hotmail.bReUseSession", true);
-
+pref("hotmail.iProcessTrigger", 50);
+pref("hotmail.iProcessAmount", 25);
