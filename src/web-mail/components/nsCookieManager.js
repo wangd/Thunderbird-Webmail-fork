204c5bc86e967848212945bc8934665b1ba0a828
archer <archer>
2006-02-05 21:39:25 +0000

    improved cookie handling

--- src/web-mail/components/nsCookieManager.js
+++ src/web-mail/components/nsCookieManager.js
@@ -13,8 +13,9 @@ function nsCookieManager()
         scriptLoader.loadSubScript("chrome://web-mail/content/common/Cookie.js");
         
         var date = new Date();
-        var  szLogFileName = "CookieManager Log - " + date.getHours()+ "-" 
-                                    + date.getMinutes() + "-"+ date.getUTCMilliseconds() +" -";
+        var  szLogFileName = "CookieManager Log - " + date.getHours()+ 
+                               "-" + date.getMinutes() + 
+                               "-"+ date.getUTCMilliseconds() +" -";
         this.m_Log = new DebugLog("webmail.logging.comms", "", szLogFileName); 
         
         this.m_Log.Write("CookieManager.js - Constructor - START");   
@@ -28,14 +29,15 @@ function nsCookieManager()
          DebugDump("CookieManager.js: Constructor : Exception : " 
                                       + e.name 
                                       + ".\nError message: " 
-                                      + e.message);
+                                      + e.message + " \n"
+                                      + e.lineNumber);
     }
 }
 
 nsCookieManager.prototype =
 {
 
-    addCookie : function (szCookie)
+    addCookie : function (url, szCookie)
     {
         try
         {
@@ -43,41 +45,143 @@ nsCookieManager.prototype =
             this.m_Log.Write("CookieManager.js - addCookie - cookie " + szCookie);
             if (!szCookie) return false; 
             
-            //remove newlines and ,
-            szCookie = szCookie.replace(/\n|,/g,";")+";";
+            var szDefaultDomain = url.prePath.match(/\/\/(.*?)$/)[1];  
+            this.m_Log.Write("CookieManager.js - addCookie - default domain " + szDefaultDomain);
             
-            //get domain
-            var szDomain = szCookie.match(/domain=(.*?);/i)[1];
-            this.m_Log.Write("CookieManager.js - addCookie - cookie domain " + szDomain);
+            //split into rows
+            var aszCookie = szCookie.split(/\n/);
+            this.m_Log.Write("CookieManager.js - addCookie - cookie rows " + aszCookie);
             
-            
-            //search cookies for domain
-            var iMax = this.m_aCookies.length;
-            if (iMax != 0)   
-            {
-                for (var i = 0 ; i<iMax ; i++)
+            //process cookies
+            var aTempCookies = new Array();
+            for (i=0; i<aszCookie.length; i++)
+            { 
+                var szDomain = szDefaultDomain; 
+                var szPath = null;
+                var iExpiry = -1;
+                var bSecure = false;
+                var szName = null;
+                var szValue = null;
+                
+                var aData = aszCookie[i].split(";");
+                this.m_Log.Write("CookieManager.js - addCookie - aData " + aData); 
+                
+                for (j=0; j<aData.length; j++)
                 {
-                    var temp = this.m_aCookies[i];  //get first item
-                    this.m_Log.Write("CookieManger.js - findCookie " + i + " "+ temp.getDomain());
-                   
-                    var regexp =  new RegExp(temp.getDomain(), "i");
-                    this.m_Log.Write("CookieManger.js - addCookie - regexp " +regexp );  
-                    if (szDomain.search(regexp)!=-1)
-                    { 
-                        temp.setCookieValue(szCookie);
-                        this.m_Log.Write("CookieManger.js - findCookie - found domain END");
-                        return true;
+                    //split name and value
+                    var iNameSplit = aData[j].indexOf("=");
+                    var szTempName = aData[j].substr(0, iNameSplit);
+                    var szTempValue = aData[j].substr(iNameSplit+1);
+                    this.m_Log.Write("CookieManager.js - addCookie ITEM - name : " + szTempName + "  value : " +szTempValue);
+                     
+                    if (szTempName.search(/domain/i)!=-1) //get domain
+                    {
+                        szDomain = szTempValue;
+                        this.m_Log.Write("CookieManager.js - addCookie - szDomain " + szDomain);
                     }
-                } 
+                    else if(szTempName.search(/path/i)!=-1) //get path
+                    {
+                        szPath = szTempValue;
+                        this.m_Log.Write("CookieManager.js - addCookie - szPath " + szPath);
+                    }
+                    else if (szTempName.search(/expires/i)!=-1)//get expiry
+                    {
+                        iExpiry = Date.parse(szTempValue.replace(/-/g," "));
+                        this.m_Log.Write("CookieManager.js - addCookie - iExpiry " + iExpiry);
+                    }
+                    else if (szTempName.search(/secure/i)!=-1)//get secure
+                    {
+                        bSecure = true;
+                        this.m_Log.Write("CookieManager.js - addCookie - bSecure " + bSecure);
+                    }
+                    else if (szTempName.search(/httponly/i)!=-1)//get httponly
+                    {
+                    }
+                    else if (szTempName.search(/version/)!=-1) //get version
+                    {
+                        if (!szName)
+                        {
+                            szName = szTempName;
+                            szValue = szTempValue;
+                        }
+                        this.m_Log.Write("CookieManager.js - addCookie - Version " + szName);
+                    }
+                    else  //should be cookie 
+                    {
+                        if (szTempName.length>0)
+                        {
+                            szName = szTempName;
+                            szValue = szTempValue;
+                            this.m_Log.Write("CookieManager.js - addCookie data - szName " + szName + " szValue " + szValue);
+                        }
+                    }
+                }
+                var oCookie = new Cookie();
+                oCookie.setDomain(szDomain); 
+                oCookie.setPath(szPath); 
+                oCookie.setName(szName);
+                oCookie.setValue(szValue); 
+                oCookie.setSecure(bSecure);
+                oCookie.setExpiry(iExpiry);
+                aTempCookies.push(oCookie);
             }
-              
-            this.m_Log.Write("CookieManger.js - findCookies - domain not found"); 
             
-            var oCookie = new Cookie( this.m_Log);
-            oCookie.newCookie(szDomain, szCookie); 
-            this.m_Log.Write("CookieManager.js - addCookie - creating new cookie"); 
-            this.m_aCookies.push(oCookie); //place cookie in array
+            //update existing cookies 
+            if (this.m_aCookies.length>0)
+            {   
+                this.m_Log.Write("CookieManger.js - addCookie - Update - Cookie m_aCookies.length " + this.m_aCookies.length);
+                var iTempCookies = aTempCookies.length;
+                for (var i=0; i<iTempCookies; i++ )
+                {    
+                    var oNewCookie = aTempCookies.shift();
+                    var szNewCookieDomain = oNewCookie.getDomain();
+                    var szNewCookieName = oNewCookie.getName();
+                    
+                    var j=0;
+                    var bFound = false;
+                    do 
+                    //for (j=0; j<this.m_aCookies.length; j++)
+                    {
+                        this.m_Log.Write("CookieManger.js - addCookie - Update - checking Cookie " + j);
+                        var temp = this.m_aCookies[j];
+                        var tempDomain = temp.getDomain();
+                        if (this.domainCheck(tempDomain,szNewCookieDomain))
+                        {
+                            this.m_Log.Write("CookieManger.js - addCookie - Update - Domain found")
+                            //found domain
+                            var tempName = temp.getName();
+                            if (this.cookieCheck(tempName,szNewCookieName))
+                            {
+                                this.m_Log.Write("CookieManger.js - addCookie - Update - Cookie found");
+                                //cookie found - update
+                                temp.setValue(oNewCookie.getValue());
+                                temp.setExpiry(oNewCookie.getExpiry());
+                                bFound = true;
+                            }
+                        }
+                        j++;
+                    }while(j!=this.m_aCookies.length && !bFound ) 
+                    //}
+                    
+                    if (bFound)
+                    {
+                        delete oNewCookie;
+                        this.m_Log.Write("CookieManger.js - addCookie - Update - deleted");
+                    }
+                    else
+                    {
+                        aTempCookies.push(oNewCookie);                        
+                        this.m_Log.Write("CookieManger.js - addCookie - Update - saved");
+                    }
+                }
+            }
             
+            //add new cookies   
+            for (var i=0; i<aTempCookies.length; i++ )
+            {
+                this.m_aCookies.push(aTempCookies[i]);
+            }
+                      
             this.m_Log.Write("CookieManager.js - addCookie - END"); 
             return true;   
         }
@@ -86,52 +190,121 @@ nsCookieManager.prototype =
              this.m_Log.Write("CookieManger.js: addCookie : Exception : " 
                                           + e.name 
                                           + ".\nError message: " 
-                                          + e.message);
+                                          + e.message + " \n"
+                                          + e.lineNumber);
         }   
     },
-    
-    
-    
-    findCookie :  function (szDomain)
+
+
+
+    findCookie :  function (url)
     {
         try
         {
             this.m_Log.Write("CookieManger.js - findCookie - START");
-            this.m_Log.Write("CookieManger.js - findCookie - target domain - " + szDomain);
-            if (!szDomain) return null;
+            var szDomain = url.prePath.match(/\/\/(.*?)$/)[1]
+            this.m_Log.Write("CookieManger.js - findCookie - domain - " + szDomain);
             
-            var iMax = this.m_aCookies.length;
-            if (iMax == 0) return null;   
-            
-            for (var i = 0 ; i<iMax ; i++)
+            var szCookies = "";
+            for (var i=0; i<this.m_aCookies.length; i++ )
             {
-                var temp = this.m_aCookies[i]
-                this.m_Log.Write("CookieManger.js - findCookie " + i + " "+ temp.getDomain());
-               
-                var regexp =  new RegExp(temp.getDomain(), "i");
-                this.m_Log.Write("CookieManger.js - findCookie - regexp " +regexp );  
-                if (szDomain.search(regexp)!=-1)
-                { 
-                    this.m_Log.Write("CookieManger.js - findCookie - found domain END"); 
-                    return temp.getCookieString();
+                if (this.domainCheck(this.m_aCookies[i].getDomain(),szDomain))
+                {
+                    var szName = this.m_aCookies[i].getName();
+                    var szValue = this.m_aCookies[i].getValue();
+                    this.m_Log.Write("CookieManger.js - findCookie - cookie - found " + szName + " " + szValue );
+                    if (szValue.length>0)
+                    {
+                        szCookies +=  szName; 
+                        szCookies += "=";
+                        szCookies +=  szValue;
+                        szCookies +=  "; " ;
+                    }
                 }
             }
-            
-            return null;
+
+            this.m_Log.Write("CookieManger.js - findCookie - szCookies " + szCookies);   
             this.m_Log.Write("CookieManger.js - findCookie - END");   
+            return szCookies;
         }
         catch(e)
         {
             this.m_Log.Write("CookieManger.js: findCookie : Exception : " 
                                           + e.name 
                                           + ".\nError message: " 
-                                          + e.message);
+                                          + e.message + " \n"
+                                          + e.lineNumber);
                                           
             return null;
         }
-    },
-
+    },    
     
+    domainCheck : function (szCookieDomain, szWantedDomain)
+    {
+        try
+        {
+            this.m_Log.Write("CookieManger.js - domainCheck - START"); 
+            this.m_Log.Write("CookieManger.js - domainCheck - cookie "+szCookieDomain + " wanted " + szWantedDomain); 
+           
+            var regexp = null;
+            var szSubject = null;
+            if (szWantedDomain.length > szCookieDomain.length)
+            { 
+                szSubject = szWantedDomain;
+                var tempDomain = szCookieDomain.replace(/^\./,"");
+                regexp =  new RegExp(tempDomain+"$", "i");
+            }
+            else
+            {  
+                szSubject = szCookieDomain;
+                var tempDomain = szWantedDomain.replace(/^\./,"");
+                regexp = new RegExp(tempDomain+"$", "i");
+            }
+           
+           
+            if (szSubject.search(regexp)!=-1)
+            { 
+                this.m_Log.Write("CookieManger.js - domainCheck - found domain END"); 
+                return true;
+            }
+            
+ 
+            return false;
+            this.m_Log.Write("CookieManger.js - domainCheck END"); 
+        }
+        catch(err)
+        {
+            return false;   
+        }
+    },
+    
+    
+    cookieCheck : function (szCookieName, szWantedName)
+    {
+        try
+        {
+            this.m_Log.Write("CookieManger.js - cookieCheck - START"); 
+            this.m_Log.Write("CookieManger.js - cookieCheck - cookie "+szCookieName + " wanted " + szWantedName); 
+            var regexp =  new RegExp("^"+szCookieName+"$", "i");
+            
+            if (szWantedName.search(regexp)!=-1)
+            {
+                this.m_Log.Write("CookieManger.js - cookieCheck - found cookie END"); 
+                return true; 
+            }
+            
+            this.m_Log.Write("CookieManger.js - cookieCheck - END"); 
+            return false;
+        }
+        catch(err)
+        {
+            return false;   
+        }
+    },
+    
+    
+
+
 /******************************************************************************/
 /***************** XPCOM  stuff ***********************************************/
 /******************************************************************************/
