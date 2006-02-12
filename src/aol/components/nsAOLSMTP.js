f55847da982e97e72191b90558078848ddca6c37
archer <archer>
2006-02-12 00:31:32 +0000

    inital upload

---
+++ src/aol/components/nsAOLSMTP.js
@@ -0,0 +1,308 @@
+/*****************************  Globals   *************************************/                 
+const nsAOLSMTPClassID = Components.ID("{411242b0-9b47-11da-a72b-0800200c9a66}");
+const nsAOLSMTPContactID = "@mozilla.org/AOLSMTP;1";
+const ExtAOLGuid = "{3c8e8390-2cf6-11d9-9669-0800200c9a66}";
+
+/******************************  AOL ***************************************/
+function nsAOLSMTP()
+{
+    try
+    {       
+        var scriptLoader =  Components.classes["@mozilla.org/moz/jssubscript-loader;1"];
+        scriptLoader = scriptLoader.getService(Components.interfaces.mozIJSSubScriptLoader);
+        scriptLoader.loadSubScript("chrome://web-mail/content/common/DebugLog.js");
+        scriptLoader.loadSubScript("chrome://web-mail/content/common/base64.js");
+        scriptLoader.loadSubScript("chrome://web-mail/content/common/comms.js");
+        scriptLoader.loadSubScript("chrome://web-mail/content/common/CommonPrefs.js");
+        
+        
+        var date = new Date();
+        var  szLogFileName = "HttpMail SMTP Log - " + date.getHours()
+                                           + "-" + date.getMinutes() 
+                                           + "-"+ date.getUTCMilliseconds() +" -";
+        this.m_Log = new DebugLog("webmail.logging.comms", ExtAOLGuid, szLogFileName); 
+        
+        this.m_Log.Write("nsAOLSMTP.js - Constructor - START");   
+       
+        this.m_bAuthorised = false;
+        this.m_szUserName = null;   
+        this.m_szPassWord = null; 
+        this.m_oResponseStream = null;  
+        this.m_HttpComms = new Comms(this , this.m_Log);
+        this.m_aszTo = new Array;
+        this.m_szFrom = null;
+        this.m_iStage = 0;
+        this.m_szSendUri = null;
+           
+        this.m_SessionManager = Components.classes["@mozilla.org/SessionManager;1"];
+        this.m_SessionManager = this.m_SessionManager.getService();
+        this.m_SessionManager.QueryInterface(Components.interfaces.nsISessionManager); 
+        this.m_SessionData = null;   
+        
+        //do i reuse the session
+        var oPref = new Object();
+        oPref.Value = null;
+        var  WebMailPrefAccess = new WebMailCommonPrefAccess();
+        if (WebMailPrefAccess.Get("bool","httpmail.bReUseSession",oPref))
+            this.m_bReUseSession=oPref.Value;
+        else
+            this.m_bReUseSession=true; 
+                  
+        //do i save copy
+        var oPref = new Object();
+        oPref.Value = null;
+        var  PrefAccess = new WebMailCommonPrefAccess();
+        if (PrefAccess.Get("bool","httpmail.bSaveCopy",oPref))
+            this.m_bSaveCopy=oPref.Value;
+        else
+            this.m_bSaveCopy=true;          
+        delete oPref;
+              
+        this.m_Log.Write("nsAOLSMTP.js - Constructor - END");  
+    }
+    catch(e)
+    {
+        DebugDump("nsAOLSMTP.js: Constructor : Exception : " 
+                                      + e.name 
+                                      + ".\nError message: " 
+                                      + e.message +"\n" +
+                                      e.lineNumber);
+    }
+}
+
+
+
+nsAOLSMTP.prototype =
+{
+    get userName() {return this.m_szUserName;},
+    set userName(userName) {return this.m_szUserName = userName;},
+  
+    get passWord() {return this.m_szPassWord;},
+    set passWord(passWord) {return this.m_szPassWord = passWord;},
+    
+    get bAuthorised() {return this.m_bAuthorised;},
+  
+    get ResponseStream() {return this.m_oResponseStream;},
+    set ResponseStream(responseStream) {return this.m_oResponseStream = responseStream;},
+    
+    get to() {return this.m_aszTo;},
+    set to(szAddress) {return this.m_aszTo.push(szAddress);},
+    
+    get from() {return this.m_szFrom;},
+    set from(szAddress) {return this.m_szFrom = szAddress;},
+    
+    
+    logIn : function()
+    {
+        try
+        {
+            this.m_Log.Write("nsAOLSMTP.js - logIN - START");   
+            this.m_Log.Write("nsAOLSMTP.js - logIN - Username: " + this.m_szUserName 
+                                                   + " Password: " + this.m_szPassWord 
+                                                   + " stream: " + this.m_oResponseStream);
+           
+            if (!this.m_szUserName || !this.m_oResponseStream  || !this.m_szPassWord) return false;
+                     
+                        
+            this.m_Log.Write("nsAOLSMTP.js - logIN - END");    
+            return true;
+        }
+        catch(e)
+        {
+            this.m_Log.DebugDump("nsAOLSMTP.js: logIN : Exception : " 
+                                              + e.name + 
+                                              ".\nError message: " 
+                                              + e.message +"\n" 
+                                              + e.lineNumber);
+                                              
+            this.serverComms("502 negative vibes from " +this.m_szUserName +"\r\n");
+            
+            return false;
+        }
+    },
+
+   
+    loginOnloadHandler : function(szResponse ,event , mainObject)
+    {
+        try
+        {
+            mainObject.m_Log.Write("nsAOLSMTP.js - loginOnloadHandler - START"); 
+            mainObject.m_Log.Write("nsAOLSMTP.js - loginOnloadHandler : " + mainObject.m_iStage );  
+            
+            var httpChannel = event.QueryInterface(Components.interfaces.nsIHttpChannel);
+            
+            //if this fails we've gone somewhere new
+            mainObject.m_Log.Write("nsAOLSMTP.js - loginOnloadHandler - status :" +httpChannel.responseStatus );
+            if (httpChannel.responseStatus != 200) 
+                throw new Error("return status " + httpChannel.responseStatus);
+           
+            mainObject.m_Log.Write("nsAOLSMTP.js - loginOnloadHandler - END");
+        }
+        catch(err)
+        {
+            mainObject.m_Log.DebugDump("nsAOLSMTP.js: loginHandler : Exception : " 
+                                          + err.name 
+                                          + ".\nError message: " 
+                                          + err.message +"\n" +
+                                            err.lineNumber);
+            mainObject.serverComms("502 negative vibes from " +mainObject.m_szUserName +"\r\n");
+        }
+    },
+    
+    
+    rawMSG : function (szEmail)
+    {
+        try
+        {
+            this.m_Log.Write("nsAOLSMTP.js - rawMSG - START");   
+            this.m_Log.Write("nsAOLSMTP.js - rawMSG " + szEmail);
+    
+            this.m_iStage = 0;
+                       
+            this.m_Log.Write("nsAOLSMTP.js - rawMSG - END");    
+            return true;
+        }
+        catch(err)
+        {
+            this.m_Log.DebugDump("nsAOLSMTP.js: rawMSG : Exception : " 
+                                              + err.name + 
+                                              ".\nError message: " 
+                                              + err.message +"\n" +
+                                                err.lineNumber);
+                                                
+            this.serverComms("502 negative vibes from " +this.m_szUserName +"\r\n");
+            
+            return false;
+        }
+    },
+    
+    
+    composerOnloadHandler : function(szResponse ,event , mainObject)
+    {
+        try
+        {
+            mainObject.m_Log.Write("nsAOLSMTP.js - composerOnloadHandler - START"); 
+            mainObject.m_Log.Write("nsAOLSMTP.js - composerOnloadHandler : " + mainObject.m_iStage + "\n");  
+            
+            var httpChannel = event.QueryInterface(Components.interfaces.nsIHttpChannel);
+            
+            //if this fails we've gone somewhere new
+            mainObject.m_Log.Write("nsAOLSMTP.js - composerOnloadHandler - status :" +httpChannel.responseStatus );
+            if (httpChannel.responseStatus != 200) 
+                throw new Error("return status " + httpChannel.responseStatus);
+           
+            mainObject.m_Log.Write("nsAOLSMTP.js - composerOnloadHandler - END");
+        }
+        catch(err)
+        {
+            mainObject.m_Log.DebugDump("nsAOLSMTP.js: composerOnloadHandler : Exception : " 
+                                          + err.name 
+                                          + ".\nError message: " 
+                                          + err.message +"\n" +
+                                            err.lineNumber);
+                                            
+            mainObject.serverComms("502 negative vibes from " +mainObject.m_szUserName +"\r\n");
+        }
+    },
+    
+    
+    ////////////////////////////////////////////////////////////////////////////
+    /////  Comms                  
+    
+    serverComms : function (szMsg)
+    {
+        try
+        { 
+            this.m_Log.Write("nsAOLSMTP.js - serverComms - START");
+            this.m_Log.Write("nsAOLSMTP.js - serverComms msg " + szMsg);
+            var iCount = this.m_oResponseStream.write(szMsg,szMsg.length);
+            this.m_Log.Write("nsAOLSMTP.js - serverComms sent count: " + iCount 
+                                                        +" msg length: " +szMsg.length);
+            this.m_Log.Write("nsAOLSMTP.js - serverComms - END");  
+        }
+        catch(e)
+        {
+            this.m_Log.DebugDump("nsAOLSMTP.js: serverComms : Exception : " 
+                                              + e.name 
+                                              + ".\nError message: " 
+                                              + e.message +"\n" +
+                                                e.lineNumber);
+        }
+    },
+    
+     
+/******************************************************************************/
+/***************** XPCOM  stuff ***********************************************/
+/******************************************************************************/
+    QueryInterface : function (iid)
+    {
+        if (!iid.equals(Components.interfaces.nsISMTPDomainHandler) 
+        	                && !iid.equals(Components.interfaces.nsISupports))
+            throw Components.results.NS_ERROR_NO_INTERFACE;
+            
+        return this;
+    }
+}
+ 
+
+/******************************************************************************/
+/* FACTORY*/
+var nsAOLSMTPFactory = new Object();
+
+nsAOLSMTPFactory.createInstance = function (outer, iid)
+{
+    if (outer != null) throw Components.results.NS_ERROR_NO_AGGREGATION;
+    
+    if (!iid.equals(nsAOLSMTPClassID) && !iid.equals(Components.interfaces.nsISupports))
+        throw Components.results.NS_ERROR_INVALID_ARG;
+    
+    return new nsAOLSMTP();
+}
+
+
+/******************************************************************************/
+/* MODULE */
+var nsAOLSMTPModule = new Object();
+
+nsAOLSMTPModule.registerSelf = function(compMgr, fileSpec, location, type)
+{
+    compMgr = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+    compMgr.registerFactoryLocation(nsAOLSMTPClassID,
+                                    "AOLSMTPComponent",
+                                    nsAOLSMTPContactID, 
+                                    fileSpec,
+                                    location, 
+                                    type);
+}
+
+
+nsAOLSMTPModule.unregisterSelf = function(aCompMgr, aFileSpec, aLocation)
+{
+    aCompMgr = aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+    aCompMgr.unregisterFactoryLocation(nsAOLSMTPClassID, aFileSpec);
+}
+
+ 
+nsAOLSMTPModule.getClassObject = function(compMgr, cid, iid)
+{
+    if (!cid.equals(nsAOLSMTPClassID))
+        throw Components.results.NS_ERROR_NO_INTERFACE;
+
+    if (!iid.equals(Components.interfaces.nsIFactory))
+        throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+
+    return nsAOLSMTPFactory;
+}
+
+
+nsAOLSMTPModule.canUnload = function(compMgr)
+{
+    return true;
+}
+/******************************************************************************/
+
+
+function NSGetModule(compMgr, fileSpec)
+{
+    return nsAOLSMTPModule; 
+}
