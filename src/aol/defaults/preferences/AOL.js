bc6945e909b9df9466d68fae075d971ec27dbe04
archer <archer>
2006-02-17 23:50:39 +0000

    removed unused prefs

--- src/aol/defaults/preferences/AOL.js
+++ src/aol/defaults/preferences/AOL.js
@@ -1,7 +1,4 @@
 pref("aol.bUseJunkMail", false);
 pref("extensions.{a6a33690-2c6a-11d9-9669-0800200c9a66}.description", "chrome://aol/locale/AOL.properties");
-pref("aol.bSaveCopy", true);
 pref("aol.bDownloadUnread", false);
 pref("aol.bReUseSession", true);
-pref("aol.iProcessTrigger", 50);
-pref("aol.iProcessAmount", 25);
