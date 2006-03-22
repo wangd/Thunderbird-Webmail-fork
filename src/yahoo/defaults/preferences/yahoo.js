45c8945bc1b288a5645aea51a34c0227087230c6
archer <archer>
2006-03-22 20:23:34 +0000

    added folders pref

--- src/yahoo/defaults/preferences/yahoo.js
+++ src/yahoo/defaults/preferences/yahoo.js
@@ -3,3 +3,4 @@ pref("yahoo.bSaveCopy", true);
 pref("extensions.{d7103710-6112-11d9-9669-0800200c9a66}.description", "chrome://yahoo/locale/Yahoo.properties");
 pref("yahoo.bDownloadUnread", false);
 pref("yahoo.bReUseSession", true);
+pref("yahoo.szFolders","");
