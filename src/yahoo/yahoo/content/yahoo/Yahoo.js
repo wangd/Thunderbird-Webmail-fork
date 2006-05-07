a9d7348b82dfc9dfdb661c19e64e5218dc888f57
archer <archer>
2006-05-07 22:47:21 +0000

    remove domain check

--- src/yahoo/yahoo/content/yahoo/Yahoo.js
+++ src/yahoo/yahoo/content/yahoo/Yahoo.js
@@ -1,14 +1,9 @@
-const cszYahooContentID = "@mozilla.org/YahooPOP;1";
-const cszYahooSMTPContentID = "@mozilla.org/YahooSMTP;1";
-
 window.addEventListener("load", function() {gYahooStartUp.init();} , false);
 
 var gYahooStartUp =
 {   
-    m_DomainManager : null,
     m_Log : null,
-    m_Timer : null,
-                            
+                        
     init : function ()
     {
         try
@@ -27,26 +22,6 @@ var gYahooStartUp =
                 return;
             }
               
-                         
-            try
-            {
-                this.m_DomainManager = Components.classes["@mozilla.org/DomainManager;1"].
-                                       getService().
-                                       QueryInterface(Components.interfaces.nsIDomainManager);
-            }
-            catch(err)
-            {
-                window.removeEventListener("load", function() {gYahooStartUp.init();} , false);
-                throw new Error("Domain Manager Not Found");
-            }
-            
-            this.m_Timer = Components.classes["@mozilla.org/timer;1"]
-                                            .createInstance(Components.interfaces.nsITimer); 
-            this.m_Timer.initWithCallback(this, 
-                                          2000, 
-                                          Components.interfaces.nsITimer.TYPE_REPEATING_SLACK);
-            this.m_Log.Write("Yahoo.js : YahooStartUp - DB not loaded");
-           
             window.removeEventListener("load", function() {gYahooStartUp.init();} , false);
         
             this.updatePrefs();
@@ -63,101 +38,7 @@ var gYahooStartUp =
     },  
        
         
-    notify: function(timer)
-    {
-        try
-        {
-            this.m_Log.Write("Yahoo.js : TimerCallback -  START");
-            
-           
-            if(!this.m_DomainManager.isReady())
-            {
-                this.m_Log.Write("Yahoo.js : TimerCallback -  db not ready");
-                return;
-            }    
-            timer.cancel();
-         
-            //get store ids
-            this.idCheck("yahoo.com","POP", cszYahooContentID );
-            this.idCheck("yahoo.es","POP", cszYahooContentID);
-            this.idCheck("yahoo.se","POP", cszYahooContentID);
-            this.idCheck("yahoo.co.jp","POP", cszYahooContentID);
-            this.idCheck("yahoo.co.uk","POP" ,cszYahooContentID);
-            this.idCheck("yahoo.it","POP", cszYahooContentID);
-            this.idCheck("yahoo.com.cn","POP", cszYahooContentID);  
-            this.idCheck("yahoo.fr","POP", cszYahooContentID);
-            this.idCheck("yahoo.de","POP", cszYahooContentID);
-            this.idCheck("yahoo.ca","POP", cszYahooContentID); 
-            this.idCheck("yahoo.com.au","POP", cszYahooContentID); 
-            this.idCheck("yahoo.com.hk","POP", cszYahooContentID); 
-            this.idCheck("talk21.com", "POP", cszYahooContentID); 
-            this.idCheck("btinternet.com", "POP" ,cszYahooContentID); 
-            this.idCheck("btopenworld.com", "POP" ,cszYahooContentID);         
-            this.idCheck("yahoo.com.sg", "POP" ,cszYahooContentID);
-            this.idCheck("yahoo.com.ar", "POP" ,cszYahooContentID);
-            this.idCheck("yahoo.ie", "POP" ,cszYahooContentID);
-            this.idCheck("yahoo.co.in", "POP" ,cszYahooContentID);
-
-            this.idCheck("yahoo.com","SMTP",cszYahooSMTPContentID);                  
-            this.idCheck("yahoo.es","SMTP", cszYahooSMTPContentID);
-            this.idCheck("yahoo.se","SMTP", cszYahooSMTPContentID);
-            this.idCheck("yahoo.co.jp","SMTP", cszYahooSMTPContentID);
-            this.idCheck("yahoo.co.uk","SMTP" ,cszYahooSMTPContentID);
-            this.idCheck("yahoo.it","SMTP", cszYahooSMTPContentID);
-            this.idCheck("yahoo.com.cn","SMTP", cszYahooSMTPContentID);  
-            this.idCheck("yahoo.fr","SMTP", cszYahooSMTPContentID);
-            this.idCheck("yahoo.de","SMTP", cszYahooSMTPContentID);
-            this.idCheck("yahoo.ca","SMTP", cszYahooSMTPContentID); 
-            this.idCheck("yahoo.com.au","SMTP", cszYahooSMTPContentID); 
-            this.idCheck("yahoo.com.hk","SMTP", cszYahooSMTPContentID); 
-            this.idCheck("talk21.com", "SMTP", cszYahooSMTPContentID); 
-            this.idCheck("btinternet.com", "SMTP" ,cszYahooSMTPContentID); 
-            this.idCheck("btopenworld.com", "SMTP" ,cszYahooSMTPContentID);    
-            this.idCheck("yahoo.com.sg", "SMTP" ,cszYahooSMTPContentID);
-            this.idCheck("yahoo.com.ar", "SMTP" ,cszYahooSMTPContentID);           
-            this.idCheck("yahoo.ie", "SMTP" ,cszYahooSMTPContentID);
-            this.idCheck("yahoo.co.in", "SMTP" ,cszYahooSMTPContentID);
-                        
-            this.m_Log.Write("Yahoo.js : TimerCallback - END");
-        }
-        catch(e)
-        {
-            this.m_Log.DebugDump("Yahoo.js : TimerCallback - Exception in notify : " 
-                                        + e.name + 
-                                        ".\nError message: " 
-                                        + e.message);
-        }
-    },
-    
-    
-    idCheck : function(szDomain, szProtocol, szYahooContentID)
-    {
-        this.m_Log.Write("Yahoo.js : idCheck - START");
-        this.m_Log.Write("Yahoo.js : idCheck - " +szDomain + " "
-                                                + szProtocol+ " "
-                                                + szYahooContentID);
-         
-        var szContentID = new Object;
-        if (this.m_DomainManager.getDomainForProtocol(szDomain,szProtocol, szContentID))
-        {
-            this.m_Log.Write("Yahoo.js : idCheck - found");
-            if (szContentID.value != szYahooContentID)
-            {
-                this.m_Log.Write("Yahoo.js : idCheck - wrong id");
-                this.m_DomainManager.newDomainForProtocol(szDomain, szProtocol, szYahooContentID);
-            }
-        }
-        else
-        {
-            this.m_Log.Write("Yahoo.js : idCheck - not found");
-            this.m_DomainManager.newDomainForProtocol(szDomain, szProtocol, szYahooContentID);    
-        }
-        
-        this.m_Log.Write("Yahoo.js : idCheck - END");
-    },
-    
-    
-    
+  
     windowCount : function()
     {
         try
