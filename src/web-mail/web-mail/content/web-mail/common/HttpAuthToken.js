1a779a18d010d4559b74733d3caf9312b32ebb26
archer <archer>
2006-05-14 23:28:15 +0000

    *** empty log message ***

--- src/web-mail/web-mail/content/web-mail/common/HttpAuthToken.js
+++ src/web-mail/web-mail/content/web-mail/common/HttpAuthToken.js
@@ -1,5 +1,6 @@
 function HttpAuthToken()
 {
+    this.m_szUserName = null;
     this.m_szDomain = null;
     this.m_szToken = null;
 }
@@ -7,6 +8,17 @@ function HttpAuthToken()
 
 HttpAuthToken.prototype =
 {
+    setUserName :function (szUserName)
+    {
+        this.m_szUserName = szUserName;
+    },
+    
+    getUserName : function ()
+    {
+        return this.m_szUserName;
+    },
+    
+    
     setDomain : function (szDomain)
     {
         this.m_szDomain = szDomain
