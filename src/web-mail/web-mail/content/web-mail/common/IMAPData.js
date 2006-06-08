20bdeb082f812878e59173b9f308bb79e81d5fa2
archer <archer>
2006-06-08 13:53:44 +0000

    increased version number

---
+++ src/web-mail/web-mail/content/web-mail/common/IMAPData.js
@@ -0,0 +1,18 @@
+function IMAPUser()
+{
+}
+
+IMAPUser.prototype.szUser = null;
+IMAPUser.prototype.aszSubFolders = null;
+IMAPUser.prototype.aFolders = null;
+
+
+
+
+
+function IMAPFolderData()
+{
+}
+
+IMAPFolderData.prototype.szHeirerchy = null;
+IMAPFolderData.prototype.szHref = null;
