756e3f2c67cfc5ce57757f53898c035724a8d311
archer <archer>
2006-06-10 09:06:36 +0000

    intial upload

--- src/web-mail/web-mail/content/web-mail/common/IMAPData.js
+++ src/web-mail/web-mail/content/web-mail/common/IMAPData.js
@@ -1,5 +1,7 @@
 function IMAPUser()
 {
+    this.aszSubFolders = new Array();
+    this.aFolders = new Array();
 }
 
 IMAPUser.prototype.szUser = null;
@@ -7,12 +9,29 @@ IMAPUser.prototype.aszSubFolders = null;
 IMAPUser.prototype.aFolders = null;
 
 
-
-
-
+/*****************/
 function IMAPFolderData()
 {
+    this.aMSG = new Array();
 }
 
 IMAPFolderData.prototype.szHeirerchy = null;
 IMAPFolderData.prototype.szHref = null;
+IMAPFolderData.prototype.szUID = null;
+IMAPFolderData.prototype.iMSGCount = 0;
+IMAPFolderData.prototype.iUnreadCount = 0;
+IMAPFolderData.prototype.aMSG= null;
+
+
+/*****************/
+function IMAPMSGData()
+{
+}
+
+IMAPMSGData.prototype.szHref = null;
+IMAPMSGData.prototype.szTo= null;
+IMAPMSGData.prototype.szFrom = null;
+IMAPMSGData.prototype.szSubject = null;
+IMAPMSGData.prototype.szDate = null;
+IMAPMSGData.prototype.bRead= false;
+IMAPMSGData.prototype.iSize= 0;
