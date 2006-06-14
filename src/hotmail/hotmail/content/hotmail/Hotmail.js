483a6810dec505084383fe0436d2b4aa5553d0d6
archer <archer>
2006-06-14 21:25:12 +0000

    removed domain register

--- src/hotmail/hotmail/content/hotmail/Hotmail.js
+++ src/hotmail/hotmail/content/hotmail/Hotmail.js
@@ -1,11 +1,7 @@
-const cszHotmailContentID = "@mozilla.org/HotmailPOP;1";
-const cszHotmailSMTPContentID = "@mozilla.org/HotmailSMTP;1";
-
 window.addEventListener("load", function() {gHotmailStartUp.init();} , false);
 
 var gHotmailStartUp =
 {   
-    m_DomainManager : null,
     m_Log : null,
     m_Timer : null,
                             
@@ -29,24 +25,6 @@ var gHotmailStartUp =
             }
               
                          
-            try
-            {
-                this.m_DomainManager = Components.classes["@mozilla.org/DomainManager;1"].
-                                       getService().
-                                       QueryInterface(Components.interfaces.nsIDomainManager);
-            }
-            catch(err)
-            {
-                window.removeEventListener("load", function() {gYahooStartUp.init();} , false);
-                throw new Error("Domain Manager Not Found");
-            }  
-           
-            this.m_Timer = Components.classes["@mozilla.org/timer;1"]
-                                            .createInstance(Components.interfaces.nsITimer); 
-            this.m_Timer.initWithCallback(this, 
-                                          2000, 
-                                          Components.interfaces.nsITimer.TYPE_REPEATING_SLACK);
-            
             //convert prefs
             var WebMailPrefAccess = new WebMailCommonPrefAccess();
             var oPref = {Value : null};
@@ -99,80 +77,8 @@ var gHotmailStartUp =
         }
     },  
        
-        
-    notify: function(timer)
-    {
-        try
-        {
-            this.m_Log.Write("Hotmail.js : notify -  START");
-            
-           
-            if(!this.m_DomainManager.isReady())
-            {
-                this.m_Log.Write("Hotmail.js : notify -  db not ready");
-                return;
-            }    
-            timer.cancel();
-         
-            //get store ids
-            this.idCheck("hotmail.com", "pop" ,cszHotmailContentID)
-            this.idCheck("hotmail.co.uk","pop" , cszHotmailContentID);
-            this.idCheck("msn.co.uk", "pop" ,cszHotmailContentID);
-            this.idCheck("msn.com", "pop" ,cszHotmailContentID);
-            this.idCheck("hotmail.it", "pop" ,cszHotmailContentID);  
-            this.idCheck("hotmail.fr", "pop" ,cszHotmailContentID);
-            this.idCheck("hotmail.de", "pop" ,cszHotmailContentID);
-            this.idCheck("hotmail.co.jp", "pop" ,cszHotmailContentID);
-            
-            this.idCheck("hotmail.com", "smtp" ,cszHotmailSMTPContentID)
-            this.idCheck("hotmail.co.uk","smtp" , cszHotmailSMTPContentID);
-            this.idCheck("msn.co.uk", "smtp" ,cszHotmailSMTPContentID);
-            this.idCheck("msn.com", "smtp" ,cszHotmailSMTPContentID);
-            this.idCheck("hotmail.it", "smtp" ,cszHotmailSMTPContentID);  
-            this.idCheck("hotmail.fr", "smtp" ,cszHotmailSMTPContentID);
-            this.idCheck("hotmail.de", "smtp" ,cszHotmailSMTPContentID)               
-            this.idCheck("hotmail.co.jp", "smtp" ,cszHotmailSMTPContentID);
-            
-            this.m_Log.Write("Hotmail.js : notify - END");
-        }
-        catch(e)
-        {
-            this.m_Log.DebugDump("Hotmail.js : notify - Exception in notify : " 
-                                        + e.name + 
-                                        ".\nError message: " 
-                                        + e.message+ "\n"
-                                        + e.lineNumber);
-        }
-    },
-    
-    
-    idCheck : function(szDomain, szProtocol, szWantedContentID)
-    {
-        this.m_Log.Write("Hotmail.js : idCheck - START");
-        this.m_Log.Write("Hotmail.js : idCheck - " +szDomain + " "
-                                                + szProtocol+ " "
-                                                + szWantedContentID);
-         
-        var szContentID = new Object;
-        if (this.m_DomainManager.getDomainForProtocol(szDomain,szProtocol, szContentID))
-        {
-            this.m_Log.Write("Hotmail.js : idCheck - found");
-            if (szContentID.value != szWantedContentID)
-            {
-                this.m_Log.Write("Hotmail.js : idCheck - wrong id");
-                this.m_DomainManager.newDomainForProtocol(szDomain, szProtocol, szWantedContentID);
-            }
-        }
-        else
-        {
-            this.m_Log.Write("Hotmail.js : idCheck - not found");
-            this.m_DomainManager.newDomainForProtocol(szDomain, szProtocol, szWantedContentID);    
-        }
-        
-        this.m_Log.Write("Hotmail.js : idCheck - END");
-    },
-    
-     windowCount : function()
+       
+    windowCount : function()
     {
         try
         {
