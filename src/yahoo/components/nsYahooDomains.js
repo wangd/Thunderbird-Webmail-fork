767aece4a55efe286f714ad2c2874b07ac3a05c7
archer <archer>
2006-06-24 18:24:14 +0000

    added custom domains

--- src/yahoo/components/nsYahooDomains.js
+++ src/yahoo/components/nsYahooDomains.js
@@ -9,14 +9,24 @@ function nsYahooDomains()
     this.m_scriptLoader = null; 
     this.m_Log = null; 
     this.m_DomainManager = null;
-    this.m_aszDomains = new Array();
-    this.m_oFile = null;
+    this.m_aszStandardDomains = new Array();
+    this.m_oStandardFile = null;
+    this.m_aszCustomDomains = new Array();
+    this.m_oCustomFile = null;
     this.m_Timer = null;
+    this.m_iFile = 0;
     this.m_bChange = false;
+    this.m_bReady = false;
 }
 
 nsYahooDomains.prototype =
 {
+    isReady : function ()
+    {
+        this.m_Log.Write("nsYahooDomains.js - isReady - " +  this.m_bReady);
+        return this.m_bReady;
+    },
+    
     addDomain : function (szDomain)
     {
         try
@@ -33,8 +43,6 @@ nsYahooDomains.prototype =
                 return false; 
             }
             
-            this.m_bChange = true;
-            this.m_aszDomains.push(szDomain);
             var bADD = false;
             
             if (!this.domainCheck(szDomain, "POP", "@mozilla.org/YahooPOP;1"))
@@ -42,6 +50,44 @@ nsYahooDomains.prototype =
             
             if (!this.domainCheck(szDomain, "SMTP", "@mozilla.org/YahooSMTP;1"))
                 bADD = this.domainAdd(szDomain, "SMTP", "@mozilla.org/YahooSMTP;1")
+            
+            var bFound = false;
+            //custom
+            if (this.m_aszCustomDomains.length>0)
+            {
+                this.m_Log.Write("nsYahooDomains.js - addDomain - search for domain - Custom"); 
+                
+                var i=0;
+                var regExp = new RegExp("^"+szDomain+"$", "i");
+                do{
+                    if (this.m_aszCustomDomains[i].search(regExp)!=-1)
+                        bFound = true;
+                    i++;
+                }while(i<this.m_aszCustomDomains.length && !bFound)                   
+            }
+            
+            //standard
+            if (!bFound && this.m_aszStandardDomains.length>0)
+            {
+                this.m_Log.Write("nsYahooDomains.js - addDomain - search for domain - Standard"); 
+                
+                var i=0;
+                var regExp = new RegExp("^"+szDomain+"$", "i");
+                do{
+                    if (this.m_aszStandardDomains[i].search(regExp)!=-1)
+                        bFound = true;
+                    i++;
+                }while(i<this.m_aszStandardDomains.length && !bFound)   
+            }
+           
+            if (!bFound)
+            {
+                this.m_Log.Write("nsYahooDomains.js - addDomain - not Found added");
+                this.m_aszCustomDomains.push(szDomain);
+            }
+                
+            this.m_bChange = true;
+
             
             this.m_Log.Write("nsYahooDomains.js - addDomain - END");
             return bADD;
@@ -80,27 +126,59 @@ nsYahooDomains.prototype =
             var regExp = new RegExp("^"+szDomain+"$", "i");
             var i=0;
             var bFound=false;
-            
-            if (this.m_aszDomains.length >0)
+            var szTempDomain;
+                        
+            //custom domains
+            if (this.m_aszCustomDomains.length >0)
             {
                 do{
-                    var temp = this.m_aszDomains.shift();
-                    this.m_Log.Write("nsYahooDomains.js - removeDomain - " + temp);
+                    szTempDomain = this.m_aszCustomDomains.shift();
+                    this.m_Log.Write("nsYahooDomains.js - removeDomain Custom- " + szTempDomain);
                     
-                    if (temp.search(regExp)==-1)
+                    if (szTempDomain.search(regExp)==-1)
                     {
-                        this.m_Log.Write("nsYahooDomains.js - removeDomain - pushed back");
-                        this.m_aszDomains.push(temp);
+                        this.m_Log.Write("nsYahooDomains.js - removeDomain Custom- pushed back");
+                        this.m_aszCustomDomains.push(szTempDomain);
                     } 
                     else
                     {   
-                        this.m_Log.Write("nsYahooDomains.js - removeDomain -found");
-                        this.m_DomainManager.removeDomainForProtocol(szDomain, "POP");   
-                        this.m_DomainManager.removeDomainForProtocol(szDomain, "SMTP");
+                        this.m_Log.Write("nsYahooDomains.js - removeDomain Custom -found");
                         bFound = true;                        
                     }
                     i++;   
-                }while(i<this.m_aszDomains.length && !bFound)
+                }while(i<this.m_aszCustomDomains.length && !bFound)
+            }
+            
+    
+            //standard domains
+            if (!bFound && this.m_aszStandardDomains.length >0)
+            {
+                do{
+                    szTempDomain = this.m_aszStandardDomains.shift();
+                    this.m_Log.Write("nsYahooDomains.js - removeDomain Standard- " + szTempDomain);
+                    
+                    if (szTempDomain.search(regExp)==-1)
+                    {
+                        this.m_Log.Write("nsYahooDomains.js - removeDomain Standard- pushed back");
+                        this.m_aszStandardDomains.push(szTempDomain);
+                    } 
+                    else
+                    {   
+                        this.m_Log.Write("nsYahooDomains.js - removeDomain Standard -found");
+                        bFound = true;                        
+                    }
+                    i++;   
+                }while(i<this.m_aszStandardDomains.length && !bFound)
+            }
+            
+            
+            
+            if (bFound)
+            {
+                this.m_Log.Write("nsYahooDomains.js - removeDomain - found");
+                this.m_DomainManager.removeDomainForProtocol(szDomain, "POP");   
+                this.m_DomainManager.removeDomainForProtocol(szDomain, "SMTP");
+                bFound = true;  
             }
             
             this.m_Log.Write("nsYahooDomains.js - removeDomain - END");
@@ -127,12 +205,7 @@ nsYahooDomains.prototype =
         {
             this.m_Log.Write("nsYahooDomains.js - getAllDomains -  START " ); 
             
-            var aResult = new Array();
-            for (i=0; i<this.m_aszDomains.length; i++)
-            {
-                aResult.push(this.m_aszDomains[i]);
-            }
-             
+            var aResult = this.m_aszStandardDomains.concat(this.m_aszCustomDomains);
             iCount.value = aResult.length;
             aszDomains.value = aResult;
             this.m_Log.Write("nsYahooDomains.js - getAllDomains - " + iCount.value + " " + aszDomains.value);
@@ -154,32 +227,35 @@ nsYahooDomains.prototype =
    
     
    
-    loadData : function()
+    loadStandardData : function()
     {
         try
         {
             this.m_Log.Write("nsYahooDomains.js - loadDataBase - START");   
-    
+            
+            this.m_iFile = 0;
+            
             //get location of DB
-            this.m_oFile = Components.classes["@mozilla.org/file/directory_service;1"].
+            this.m_oStandardFile = Components.classes["@mozilla.org/file/directory_service;1"].
                                       createInstance(Components.interfaces.nsIProperties).
                                       get("ProfD", Components.interfaces.nsIFile);                                 
-            this.m_oFile.append("extensions");          //goto profile extension folder
-            this.m_oFile.append("{d7103710-6112-11d9-9669-0800200c9a66}"); //goto client extension folder
-            this.m_oFile.append("domains.txt");       //goto logfiles folder
+            this.m_oStandardFile.append("extensions");          //goto profile extension folder
+            this.m_oStandardFile.append("{d7103710-6112-11d9-9669-0800200c9a66}"); //goto client extension folder
+            this.m_oStandardFile.append("domains.txt");       //goto logfiles folder
+            
             
             //check file exist  
-            if (!this.m_oFile.exists())
+            if (!this.m_oStandardFile.exists())
             {   //create file
-                this.m_Log.Write("nsYahooDomains.js - loadDataBase - creating file");   
-                this.m_oFile.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 420);
+                this.m_Log.Write("nsYahooDomains.js - loadStandardData - creating file");   
+                this.m_oStandardFile.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 420);
             }
             
             //open file  
             var ioService = Components.classes["@mozilla.org/network/io-service;1"].
                                     getService(Components.interfaces.nsIIOService);
                        
-            var fileURI = ioService.newFileURI(this.m_oFile);
+            var fileURI = ioService.newFileURI(this.m_oStandardFile);
             var channel = ioService.newChannelFromURI(fileURI); 
              
             var streamLoader = Components.classes["@mozilla.org/network/stream-loader;1"].
@@ -201,6 +277,57 @@ nsYahooDomains.prototype =
         }
     },
 
+    
+    
+    loadCustomData : function()
+    {
+        try
+        {
+            this.m_Log.Write("nsYahooDomains.js - loadCustomData - START");   
+            
+            this.m_iFile = 1;
+            
+            //get location of DB
+            this.m_oCustomFile = Components.classes["@mozilla.org/file/directory_service;1"].
+                                      createInstance(Components.interfaces.nsIProperties).
+                                      get("ProfD", Components.interfaces.nsIFile);
+            this.m_oCustomFile.append("WebmailData");          //goto data folder
+            this.m_oCustomFile.append("YahooDomains.txt");       //goto domains
+            
+            //check file exist  
+            if (!this.m_oCustomFile.exists())
+            {   //create file
+                this.m_Log.Write("nsYahooDomains.js - loadCustomData - creating file");   
+                this.m_oCustomFile.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 420);
+            }
+            
+            //open file  
+            var ioService = Components.classes["@mozilla.org/network/io-service;1"].
+                                    getService(Components.interfaces.nsIIOService);
+                       
+            var fileURI = ioService.newFileURI(this.m_oCustomFile);
+            var channel = ioService.newChannelFromURI(fileURI); 
+             
+            var streamLoader = Components.classes["@mozilla.org/network/stream-loader;1"].
+                                    createInstance(Components.interfaces.nsIStreamLoader);
+            streamLoader.init(channel, this, null); 
+            
+            this.m_Log.Write("nsYahooDomains.js - loadCustomData - END");  
+            return true;
+        }
+        catch(err)
+        {
+            this.m_Log.DebugDump("nsYahooDomains.js: loadCustomData : Exception : " 
+                                          + err.name 
+                                          + ".\nError message: " 
+                                          + err.message + "\n"
+                                          + err.lineNumber);
+                                          
+            return false;
+        }
+    },
+    
+    
     
     onStreamComplete : function(aLoader, aContext, aStatus, ResultLength, Result)
     { 
@@ -236,15 +363,23 @@ nsYahooDomains.prototype =
                     {   
                         var szDomain = aszRows[i].match(/<domain>([\S\s]*?)<\/domain>/i)[1];
                         this.m_Log.Write("nsYahooDomains.js - onStreamComplete - szDomain " + szDomain);   
-                        this.m_aszDomains.push(szDomain);       
+                        if (this.m_iFile==0)
+                            this.m_aszStandardDomains.push(szDomain);   
+                        else    
+                            this.m_aszCustomDomains.push(szDomain);              
                     }
                     
-                    //assume DB not ready start timer  
-                    this.m_Timer = Components.classes["@mozilla.org/timer;1"]
-                                            .createInstance(Components.interfaces.nsITimer); 
-                    this.m_Timer.initWithCallback(this, 
-                                                  100, 
-                                                  Components.interfaces.nsITimer.TYPE_REPEATING_SLACK);
+                    if (this.m_iFile ==0)
+                        this.loadCustomData();
+                    else
+                    {
+                        //assume DB not ready start timer  
+                        this.m_Timer = Components.classes["@mozilla.org/timer;1"]
+                                                .createInstance(Components.interfaces.nsITimer); 
+                        this.m_Timer.initWithCallback(this, 
+                                                      100, 
+                                                      Components.interfaces.nsITimer.TYPE_REPEATING_SLACK);
+                    }
                 }
             }
             catch(err)
@@ -271,6 +406,8 @@ nsYahooDomains.prototype =
     },
     
     
+    
+    
     notify: function(timer)
     {
         try
@@ -285,13 +422,16 @@ nsYahooDomains.prototype =
             }    
             timer.cancel();
             
-            for (i=0; i < this.m_aszDomains.length; i++)
+            var aszDomain = this.m_aszCustomDomains.concat(this.m_aszCustomDomains);
+            for (i=0; i < aszDomain.length; i++)
             {   
-                if (!this.domainCheck(this.m_aszDomains[i], "POP", "@mozilla.org/YahooPOP;1"))
-                    this.domainAdd(this.m_aszDomains[i], "POP", "@mozilla.org/YahooPOP;1")      
-                if (!this.domainCheck(this.m_aszDomains[i], "SMTP", "@mozilla.org/YahooSMTP;1"))
-                    this.domainAdd(this.m_aszDomains[i], "SMTP", "@mozilla.org/YahooSMTP;1")
+                if (!this.domainCheck(aszDomain[i], "POP", "@mozilla.org/YahooPOP;1"))
+                    this.domainAdd(aszDomain[i], "POP", "@mozilla.org/YahooPOP;1")      
+                if (!this.domainCheck(aszDomain[i], "SMTP", "@mozilla.org/YahooSMTP;1"))
+                    this.domainAdd(aszDomain[i], "SMTP", "@mozilla.org/YahooSMTP;1")
             }
+            
+            this.m_bReady = true;
                         
             this.m_Log.Write("nsYahooDomains.js : TimerCallback - END");
         }
@@ -375,16 +515,31 @@ nsYahooDomains.prototype =
         {
             this.m_Log.Write("nsYahooDomains.js - saveDataBase - START");
 
+           //create standard file
             var szDataBase = "<database>\r\n";
-            for (j=0; j<this.m_aszDomains.length; j++)
+            for (j=0; j<this.m_aszStandardDomains.length; j++)
             {
-                szDataBase += "<domain>"+this.m_aszDomains[j]+"</domain>\r\n";
+                szDataBase += "<domain>"+this.m_aszStandardDomains[j]+"</domain>\r\n";
             }
             szDataBase += "</database>\r\n";
             
             var outStream = Components.classes["@mozilla.org/network/file-output-stream;1"]
                                 .createInstance(Components.interfaces.nsIFileOutputStream);
-            outStream.init(this.m_oFile, 0x04 | 0x20 , 420 , 0); 
+            outStream.init(this.m_oStandardFile, 0x04 | 0x20 , 420 , 0); 
+            outStream.write( szDataBase, szDataBase.length );
+            outStream.close();
+            
+            ///create custom file
+            szDataBase = "<database>\r\n";
+            for (j=0; j<this.m_aszCustomDomains.length; j++)
+            {
+                szDataBase += "<domain>"+this.m_aszCustomDomains[j]+"</domain>\r\n";
+            }
+            szDataBase += "</database>\r\n";
+            
+            var outStream = Components.classes["@mozilla.org/network/file-output-stream;1"]
+                                .createInstance(Components.interfaces.nsIFileOutputStream);
+            outStream.init(this.m_oCustomFile, 0x04 | 0x20 , 420 , 0); 
             outStream.write( szDataBase, szDataBase.length );
             outStream.close();
             
@@ -441,7 +596,7 @@ nsYahooDomains.prototype =
                     this.m_Log.Write("nsYahooDomains.js - domainmanager not found");
                 }
                 
-                this.loadData();
+                this.loadStandardData();
             break;
 
             case "quit-application":
