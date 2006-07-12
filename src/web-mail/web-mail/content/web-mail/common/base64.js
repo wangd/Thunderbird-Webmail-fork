0bcee124dce56c378de01a3f0b75a77954c10551
archer <archer>
2006-07-12 15:52:31 +0000

    added line break

--- src/web-mail/web-mail/content/web-mail/common/base64.js
+++ src/web-mail/web-mail/content/web-mail/common/base64.js
@@ -8,11 +8,35 @@ function base64()
     this.m_HiddenWindow = this.m_hShellService.hiddenDOMWindow;
 }
 
+base64.prototype.bLineBreak = false;
 
+base64.prototype.iLineBreak = 74;
 
 base64.prototype.encode = function(szMsg)
 {
-    return this.m_HiddenWindow.btoa(szMsg);
+    var szB6Temp = this.m_HiddenWindow.btoa(szMsg);
+      
+    //check length if geater than 74 char split#
+    var szB6MSG = "";
+    
+    if (this.bLineBreak)
+    {
+        if (szB6Temp.length < 300) 
+            szB6MSG = szB6Temp;
+        else
+        {  
+            for (var i=0; i<szB6Temp.length; i+=this.iLineBreak)
+            {
+                szB6MSG += szB6Temp.substr(i, this.iLineBreak);
+                szB6MSG +=  "\r\n"
+            } 
+        }
+     }
+     else
+        szB6MSG = szB6Temp;
+  
+    
+    return szB6MSG;
 }
 
 
