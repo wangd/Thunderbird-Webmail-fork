a897b7fa74e1e1ba3b5f244332b8ed648e5bd168
archer <archer>
2007-05-23 18:39:08 +0000

    *** empty log message ***

--- src/aol/defaults/preferences/AOL.js
+++ src/aol/defaults/preferences/AOL.js
@@ -1,2 +1,4 @@
 pref("extensions.{268d7420-9032-11da-a72b-0800200c9a66}.description", "chrome://aol/locale/AOL.properties");
 pref("aol.bReUseSession", true);
+pref("aol.iProcessAmount", 25);
+pref("aol.iProcessDelay", 10);
