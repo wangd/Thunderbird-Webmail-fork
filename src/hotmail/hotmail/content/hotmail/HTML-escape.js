fa814f25aaa36e2eb847e7dbe7d20ffd8b521d9a
archer <archer>
2007-09-08 16:03:47 +0000

    added other HTML code MS uses

--- src/hotmail/hotmail/content/hotmail/HTML-escape.js
+++ src/hotmail/hotmail/content/hotmail/HTML-escape.js
@@ -12,6 +12,7 @@ function HTMLescape()
                              
     this.kBlockSize = 5000;
     this.kTerminator = "&#13;&#10;";
+    this.kTerminatorAlt = "\r\n";
     this.m_iCurrentRead = 0;
     this.m_iStreamSize = 0;
     this.m_inStream = null;
@@ -53,31 +54,41 @@ HTMLescape.prototype.decode = function(rawMSG)
                      .replace(/&#61;/gm, "=");
                      
         //might be there
-        if (szMSG.search(/&#33;/gm)!=-1) szMSG = szMSG.replace(/&#33;/gm, "!");        
-        if (szMSG.search(/&#35;/gm)!=-1) szMSG = szMSG.replace(/&#35;/gm, "#");
-        if (szMSG.search(/&#36;/gm)!=-1) szMSG = szMSG.replace(/&#36;/gm, "$"); 
-        if (szMSG.search(/&#37;/gm)!=-1) szMSG = szMSG.replace(/&#37;/gm, "%");
-        if (szMSG.search(/&#38;/gm)!=-1) szMSG = szMSG.replace(/&#38;/gm, "&");
-        if (szMSG.search(/&#39;/gm)!=-1) szMSG = szMSG.replace(/&#39;/gm, "'");
-        if (szMSG.search(/&#40;/gm)!=-1) szMSG = szMSG.replace(/&#40;/gm, "(");
-        if (szMSG.search(/&#41;/gm)!=-1) szMSG = szMSG.replace(/&#41;/gm, ")");
-        if (szMSG.search(/&#42;/gm)!=-1) szMSG = szMSG.replace(/&#42;/gm, "*");
-        if (szMSG.search(/&#43;/gm)!=-1) szMSG = szMSG.replace(/&#43;/gm, "+");
-        if (szMSG.search(/&#47;/gm)!=-1) szMSG = szMSG.replace(/&#47;/gm, "/");
-        if (szMSG.search(/&#60;/gm)!=-1) szMSG = szMSG.replace(/&#60;/gm, "<");
-        if (szMSG.search(/&#62;/gm)!=-1) szMSG = szMSG.replace(/&#62;/gm, ">");
-        if (szMSG.search(/&#63;/gm)!=-1) szMSG = szMSG.replace(/&#63;/gm, "?");
-        if (szMSG.search(/&#64;/gm)!=-1) szMSG = szMSG.replace(/&#64;/gm, "@");
-        if (szMSG.search(/&#91;/gm)!=-1) szMSG = szMSG.replace(/&#91;/gm, "[");
-        if (szMSG.search(/&#92;/gm)!=-1) szMSG = szMSG.replace(/&#92;/gm, "\\");
-        if (szMSG.search(/&#93;/gm)!=-1) szMSG = szMSG.replace(/&#93;/gm, "]");
-        if (szMSG.search(/&#94;/gm)!=-1) szMSG = szMSG.replace(/&#94;/gm, "^");
-        if (szMSG.search(/&#95;/gm)!=-1) szMSG = szMSG.replace(/&#95;/gm, "_");
-        if (szMSG.search(/&#96;/gm)!=-1) szMSG = szMSG.replace(/&#96;/gm, "`");
-        if (szMSG.search(/&#123;/gm)!=-1) szMSG = szMSG.replace(/&#123;/gm, "{");
-        if (szMSG.search(/&#124;/gm)!=-1) szMSG = szMSG.replace(/&#124;/gm, "|");
-        if (szMSG.search(/&#125;/gm)!=-1) szMSG = szMSG.replace(/&#125;/gm, "}");
-        if (szMSG.search(/&#126;/gm)!=-1) szMSG = szMSG.replace(/&#126;/gm, "~");
+        if (szMSG.search(/&#33;/g)!=-1) szMSG = szMSG.replace(/&#33;/gm, "!");        
+        if (szMSG.search(/&#35;/g)!=-1) szMSG = szMSG.replace(/&#35;/gm, "#");
+        if (szMSG.search(/&#36;/g)!=-1) szMSG = szMSG.replace(/&#36;/gm, "$"); 
+        if (szMSG.search(/&#37;/g)!=-1) szMSG = szMSG.replace(/&#37;/gm, "%");
+        if (szMSG.search(/&#38;/g)!=-1) szMSG = szMSG.replace(/&#38;/gm, "&");
+        if (szMSG.search(/&#39;/g)!=-1) szMSG = szMSG.replace(/&#39;/gm, "'");
+        if (szMSG.search(/&#40;/g)!=-1) szMSG = szMSG.replace(/&#40;/gm, "(");
+        if (szMSG.search(/&#41;/g)!=-1) szMSG = szMSG.replace(/&#41;/gm, ")");
+        if (szMSG.search(/&#42;/g)!=-1) szMSG = szMSG.replace(/&#42;/gm, "*");
+        if (szMSG.search(/&#43;/g)!=-1) szMSG = szMSG.replace(/&#43;/gm, "+");
+        if (szMSG.search(/&#47;/g)!=-1) szMSG = szMSG.replace(/&#47;/gm, "/");
+        if (szMSG.search(/&#60;/g)!=-1) szMSG = szMSG.replace(/&#60;/gm, "<");
+        if (szMSG.search(/&#62;/g)!=-1) szMSG = szMSG.replace(/&#62;/gm, ">");
+        if (szMSG.search(/&#63;/g)!=-1) szMSG = szMSG.replace(/&#63;/gm, "?");
+        if (szMSG.search(/&#64;/g)!=-1) szMSG = szMSG.replace(/&#64;/gm, "@");
+        if (szMSG.search(/&#91;/g)!=-1) szMSG = szMSG.replace(/&#91;/gm, "[");
+        if (szMSG.search(/&#92;/g)!=-1) szMSG = szMSG.replace(/&#92;/gm, "\\");
+        if (szMSG.search(/&#93;/g)!=-1) szMSG = szMSG.replace(/&#93;/gm, "]");
+        if (szMSG.search(/&#94;/g)!=-1) szMSG = szMSG.replace(/&#94;/gm, "^");
+        if (szMSG.search(/&#95;/g)!=-1) szMSG = szMSG.replace(/&#95;/gm, "_");
+        if (szMSG.search(/&#96;/g)!=-1) szMSG = szMSG.replace(/&#96;/gm, "`");
+        if (szMSG.search(/&#123;/g)!=-1) szMSG = szMSG.replace(/&#123;/gm, "{");
+        if (szMSG.search(/&#124;/g)!=-1) szMSG = szMSG.replace(/&#124;/gm, "|");
+        if (szMSG.search(/&#125;/g)!=-1) szMSG = szMSG.replace(/&#125;/gm, "}");
+        if (szMSG.search(/&#126;/g)!=-1) szMSG = szMSG.replace(/&#126;/gm, "~");
+
+        //some more MS use
+        if (szMSG.search(/&lt;/g)!=-1) szMSG = szMSG.replace(/&lt;/gm,"<");
+        if (szMSG.search(/&gt;/g)!=-1) szMSG = szMSG.replace(/&gt;/gm,">");
+        if (szMSG.search(/&quot;/g)!=-1) szMSG = szMSG.replace(/&quot;/gm,"\"");
+        if (szMSG.search(/&amp;/g)!=-1) szMSG = szMSG.replace(/&amp;/gm,"&");
+        if (szMSG.search(/&nbsp;/g)!=-1) szMSG = szMSG.replace(/&nbsp;/gm," ");
+        if (szMSG.search(/&apos;/g)!=-1) szMSG = szMSG.replace(/&apos;/gm,"\'");
+        if (szMSG.search(/\r/g)!=-1) szMSG = szMSG.replace(/\r/gm,"");
+        if (szMSG.search(/\n/g)!=-1) szMSG = szMSG.replace(/\n/gm,"\r\n");
 
         return szMSG;
     }
@@ -93,7 +104,7 @@ HTMLescape.prototype.decode = function(rawMSG)
 }
 
 
-HTMLescape.prototype.largeDecode = function(rawMSG, callback, parent)
+HTMLescape.prototype.decodeAsync = function(rawMSG, callback, parent)
 {
     try
     {
@@ -113,8 +124,8 @@ HTMLescape.prototype.largeDecode = function(rawMSG, callback, parent)
         this.m_iCurrentRead = 0;
         this.m_szTempMSG = "";
         this.m_szCleanMSG = "";
-        this.m_iStreamSize = this.m_inStream.available();
-        this.m_Log.Write("HTMLescape.js - largeDecode - Size " + this.m_iStreamSize);
+        //this.m_iStreamSize = this.m_inStream.available();
+        this.m_Log.Write("HTMLescape.js - largeDecode - Size " + this.m_inStream.available());
         
         this.m_Timer.initWithCallback(this,
                                       10,
@@ -145,11 +156,17 @@ HTMLescape.prototype.notify = function (timer)
         this.m_szTempMSG += this.m_inStream.read(this.kBlockSize);
         this.m_Log.Write("HTMLescape.js - notify - Raw Block \n" + this.m_szTempMSG);
                 
-        this.m_iCurrentRead += this.kBlockSize;
-        this.m_Log.Write("HTMLescape.js - notify - Block Size " + this.m_iCurrentRead);               
+        //this.m_iCurrentRead += this.kBlockSize;
+        this.m_Log.Write("HTMLescape.js - notify - Block Size " + this.m_szTempMSG.length);               
         
         //clean only complete lines
-        var iComplete = this.m_szTempMSG.lastIndexOf(this.kTerminator )+ this.kTerminator.length;
+        var iComplete = this.m_szTempMSG.lastIndexOf(this.kTerminator )
+        if (iComplete ==-1)
+            iComplete = this.m_szTempMSG.lastIndexOf(this.kTerminatorAlt)+ this.kTerminatorAlt.length;
+        else
+            iComplete += this.kTerminator.length
+        this.m_Log.Write("HTMLescape.js - notify - iComplete " + iComplete);    
+
         var szToClean = this.m_szTempMSG.substr(0,iComplete );
         var szDirty = this.m_szTempMSG.substr(iComplete, this.m_szTempMSG.length-iComplete);
         this.m_szTempMSG = szDirty;
@@ -157,7 +174,7 @@ HTMLescape.prototype.notify = function (timer)
         this.m_szCleanMSG += this.decode(szToClean); 
         
         //stream completed
-        if (this.m_iCurrentRead >= this.m_iStreamSize)  
+        if (this.m_inStream.available() == 0)  
         {
             timer.cancel();
             this.m_inStream.close();
