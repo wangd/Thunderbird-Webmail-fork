8a694490e5e5e105165ae745239c7780dcfd57fb
archer <archer>
2008-09-22 17:49:18 +0000

    updated about box

--- src/gmail/gmail/content/gmail/GMail-About.js
+++ src/gmail/gmail/content/gmail/GMail-About.js
@@ -6,22 +6,57 @@ var gGMailAbout =
     m_iElementCount : 2,
     m_iTime : 3000,
 
+
     init : function()
     {
-        var strBundle = document.getElementById("stringsGMailAbout");
-        var szContributors = strBundle.getString("ExtContributorNames");
-        var aszNames = szContributors.split(";");
-        var list = document.getElementById("GMailContributorBox");
+        var extensionDS = Components.classes["@mozilla.org/extensions/manager;1"]
+                                    .getService(Components.interfaces.nsIExtensionManager)
+                                    .datasource;
+     
+        var rdfs = Components.classes["@mozilla.org/rdf/rdf-service;1"]
+                             .getService(Components.interfaces.nsIRDFService);
+        var extension = rdfs.GetResource("urn:mozilla:item:{860a7040-44a3-11da-8cd6-0800200c9a66}");
+
+        // Version
+        var versionArc = rdfs.GetResource("http://www.mozilla.org/2004/em-rdf#version");
+        var version = extensionDS.GetTarget(extension, versionArc, true);
+        version = version.QueryInterface(Components.interfaces.nsIRDFLiteral).Value;   
+        document.getElementById("GMailVersion").setAttribute("value", version);
+
+        //creator 
+        var creatorArc = rdfs.GetResource("http://www.mozilla.org/2004/em-rdf#creator");
+        var creator = extensionDS.GetTarget(extension, creatorArc, true);
+        creator = creator.QueryInterface(Components.interfaces.nsIRDFLiteral).Value;
+        document.getElementById("GMailCreatorLabel").setAttribute("value", creator);
+
+        //URL
+        var homepageArc = rdfs.GetResource("http://www.mozilla.org/2004/em-rdf#homepageURL");
+        var homepage = extensionDS.GetTarget(extension, homepageArc, true);
+        homepage = homepage.QueryInterface(Components.interfaces.nsIRDFLiteral).Value;
+        var szHomePage = "window.opener.openURL('" + homepage +"');";
+        document.getElementById("GMailHomePage").setAttribute("onclick", szHomePage);     
+        document.getElementById("GMailHomePage").setAttribute("tooltiptext", homepage); 
+
+         //Contributor
+        var contributorsArc = rdfs.GetResource("http://www.mozilla.org/2004/em-rdf#contributor");
+        var contributors = extensionDS.GetTargets(extension, contributorsArc, true);
+        var aszNames = new Array();
+        while (contributors.hasMoreElements()) 
+        {
+            var contributor = contributors.getNext().QueryInterface(Components.interfaces.nsIRDFLiteral).Value;
+            aszNames.push(contributor);
+        }
         
+        var list = document.getElementById("GMailContributorBox");
+
         this.m_Timer = Components.classes["@mozilla.org/timer;1"];
         this.m_Timer = this.m_Timer.createInstance(Components.interfaces.nsITimer); 
-        
+
         if (aszNames.length > this.m_iElementCount)
             this.createDeck(list, aszNames);
         else
             this.createList(list, aszNames);  
-        
-    },
+     },
 
 
     createDeck : function(base, aszNames)
@@ -34,7 +69,7 @@ var gGMailAbout =
         var iCount = 0;
                 
         for(i =0 ; i< aszNames.length; i++)
-        {
+        {   
             if (iCount == 0) vbox = document.createElement("vbox"); 
             var hBox = this.processListItem(aszNames[i]);
             vbox.appendChild(hBox);
@@ -108,5 +143,5 @@ var gGMailAbout =
     stop : function ()
     {
         this.m_Timer.cancel();
-    },
+    }
 };
