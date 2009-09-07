be4dc6eaa5a66dc5c890c3a4fc5c07381bb158cb
archer <archer>
2009-09-07 18:54:38 +0000

    Increased version number

--- src/yahoo/install.js
+++ src/yahoo/install.js
@@ -6,7 +6,7 @@
 
 // Editable Items Begin
 var displayName         = "WebMail - Yahoo"; // The name displayed to the user (don't include the version)
-var version             = "1.4.4";
+var version             = "1.4.5";
 var name                = "yahoo"; // The leafname of the JAR file (without the .jar part)
 
 // The following three sets of variables tell this installer script how your
